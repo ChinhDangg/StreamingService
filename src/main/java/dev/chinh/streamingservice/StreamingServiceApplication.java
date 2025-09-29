6bd8981ec8b0e5816a9f3b3e723f1398a7ccc143
chinhdang <dangchinh645@gmail.com>
2025-09-29 15:04:42 -0400

    add service for minio

--- src/main/java/dev/chinh/streamingservice/StreamingServiceApplication.java
+++ src/main/java/dev/chinh/streamingservice/StreamingServiceApplication.java
@@ -1,7 +1,10 @@
 package dev.chinh.streamingservice;
 
+import dev.chinh.streamingservice.content.service.MinIOService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StreamingServiceApplication {
@@ -10,4 +13,9 @@ public class StreamingServiceApplication {
         SpringApplication.run(StreamingServiceApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(MinIOService minIOService) {
+        return args -> {
+        };
+    }
 }
---
+++ src/main/java/dev/chinh/streamingservice/content/service/MinIOService.java
@@ -0,0 +1,39 @@
+package dev.chinh.streamingservice.content.service;
+
+import io.minio.GetPresignedObjectUrlArgs;
+import io.minio.ListObjectsArgs;
+import io.minio.MinioClient;
+import io.minio.Result;
+import io.minio.http.Method;
+import io.minio.messages.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MinIOService {
+
+    private final MinioClient minioClient;
+
+    public String getSignedUrl(String bucket, String object, int expirySeconds) throws Exception {
+        return minioClient.getPresignedObjectUrl(
+                GetPresignedObjectUrlArgs.builder()
+                        .method(Method.GET)
+                        .bucket(bucket)
+                        .object(object)
+                        .expiry(expirySeconds)
+                        .build()
+        );
+    }
+
+    public Iterable<Result<Item>> getAllItemsInBucketWithPrefix(String bucketName, String prefix) {
+        return minioClient.listObjects(
+                ListObjectsArgs.builder()
+                        .bucket(bucketName)
+                        .prefix(prefix)
+                        .recursive(true)    // list everything under this prefix
+                        .build()
+        );
+    }
+
+}
