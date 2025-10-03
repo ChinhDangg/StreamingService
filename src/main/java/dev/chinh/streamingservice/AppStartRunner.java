2ed4691ea275a90c316703b5197922e87b043bdc
chinhdang <dangchinh645@gmail.com>
2025-10-02 20:38:37 -0400

    add app start runner to create ram disk if not found and then start docker compose second

---
+++ src/main/java/dev/chinh/streamingservice/AppStartRunner.java
@@ -0,0 +1,22 @@
+package dev.chinh.streamingservice;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppStartRunner implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("SOmeting");
+        boolean ramCreated = OSUtil.createRamDisk();
+
+        if (!ramCreated) {
+            System.out.println("Fail to create RAM DISK");
+            return;
+        }
+
+        OSUtil.startDockerCompose();
+    }
+}
