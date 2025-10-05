568fb988b3ad7bd7e2301cb01a2315186f2cd456
chinhdang <dangchinh645@gmail.com>
2025-10-05 00:37:35 -0400

    refactor media search service to do the mapping from the searchResponse and handle caching

--- src/main/java/dev/chinh/streamingservice/search/service/MediaSearchService.java
+++ src/main/java/dev/chinh/streamingservice/search/service/MediaSearchService.java
@@ -1,9 +1,13 @@
 package dev.chinh.streamingservice.search.service;
 
-import dev.chinh.streamingservice.search.MediaSearchItem;
-import dev.chinh.streamingservice.search.MediaSearchRequest;
-import dev.chinh.streamingservice.search.MediaSearchResult;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.chinh.streamingservice.search.data.MediaSearchItem;
+import dev.chinh.streamingservice.search.data.MediaSearchItemResponse;
+import dev.chinh.streamingservice.search.data.MediaSearchRequest;
+import dev.chinh.streamingservice.search.data.MediaSearchResult;
 import lombok.RequiredArgsConstructor;
+import org.opensearch.action.search.SearchResponse;
+import org.opensearch.search.SearchHit;
 import org.opensearch.search.sort.SortOrder;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -19,6 +23,7 @@ public class MediaSearchService {
 
     private final RedisTemplate<String, Object> redisTemplate;
     private final OpenSearchService openSearchService;
+    private final ObjectMapper mapper;
 
     private void cacheMediaSearchItem(MediaSearchItem item) {
         String id = "media::" + item.getId();
@@ -50,33 +55,63 @@ public class MediaSearchService {
                 fieldValues.put(fieldName, Collections.singletonList(value));
             }
         }
+        MapSearchResult mapSearchResult = mapResponseToMediaSearchResult(
+                openSearchService.advanceSearch(fieldValues, page, size, sortByYear, sortOrder), page, size);
 
-        MediaSearchResult result =  openSearchService.advanceSearch(fieldValues, page, size, sortByYear, sortOrder);
-        cacheMediaSearchItems(result.getSearchItems());
-        return result;
+        cacheMediaSearchItems(mapSearchResult.searchItems);
+        return mapSearchResult.searchResult;
     }
 
     public MediaSearchResult search(String searchString, int page, int size, boolean sortByYear,
                                     SortOrder sortOrder) throws IOException {
 
-        MediaSearchResult result = openSearchService.search(searchString, page, size, sortByYear, sortOrder);
-        cacheMediaSearchItems(result.getSearchItems());
-        return result;
+        if (!MediaSearchRequest.validateSearchString(searchString)) {
+            throw new IllegalArgumentException("Invalid search string");
+        }
+        MapSearchResult mapSearchResult = mapResponseToMediaSearchResult(
+                openSearchService.search(searchString, page, size, sortByYear, sortOrder), page, size);
+
+        cacheMediaSearchItems(mapSearchResult.searchItems);
+        return mapSearchResult.searchResult;
     }
 
     public MediaSearchResult searchByKeywords(String field, Collection<Object> keywords, int page, int size, boolean sortByYear,
                                     SortOrder sortOrder) throws IOException {
         MediaSearchRequest.validateFieldName(field);
-        MediaSearchResult result =  openSearchService.searchTermByOneField(field, keywords, page, size, sortByYear, sortOrder);
-        cacheMediaSearchItems(result.getSearchItems());
-        return result;
+        MapSearchResult mapResult = mapResponseToMediaSearchResult(
+                openSearchService.searchTermByOneField(field, keywords, page, size, sortByYear, sortOrder), page, size);
+        cacheMediaSearchItems(mapResult.searchItems);
+        return mapResult.searchResult;
     }
 
-    public MediaSearchResult searchMatch(String field, Collection<String> searchStrings, int page, int size, boolean sortByYear,
+    public MediaSearchResult searchMatch(String field, Collection<Object> searchStrings, int page, int size, boolean sortByYear,
                                          SortOrder sortOrder) throws IOException {
         MediaSearchRequest.validateFieldName(field);
-        MediaSearchResult result = openSearchService.searchMatchByOneField(field, searchStrings, page, size, sortByYear, sortOrder);
-        cacheMediaSearchItems(result.getSearchItems());
-        return result;
+        MapSearchResult mapResult = mapResponseToMediaSearchResult(
+                openSearchService.searchMatchByOneField(field, searchStrings, page, size, sortByYear, sortOrder), page, size);
+        cacheMediaSearchItems(mapResult.searchItems);
+        return mapResult.searchResult;
+    }
+
+    public record MapSearchResult(
+            MediaSearchResult searchResult,
+            Collection<MediaSearchItem> searchItems
+    ) {}
+
+    private MapSearchResult mapResponseToMediaSearchResult(SearchResponse response, int page, int size) {
+        List<MediaSearchItem> items = new ArrayList<>();
+        List<MediaSearchItemResponse> itemResponses = new ArrayList<>();
+        for (SearchHit hit : response.getHits()) {
+            items.add(mapper.convertValue(hit.getSourceAsMap(), MediaSearchItem.class));
+            itemResponses.add(mapper.convertValue(hit.getSourceAsMap(), MediaSearchItemResponse.class));
+        }
+
+        MediaSearchResult result = new MediaSearchResult(itemResponses);
+        result.setPage(page);
+        result.setPageSize(size);
+        result.setTotal(Objects.requireNonNull(response.getHits().getTotalHits()).value());
+        result.setTotalPages((result.getTotal() + size -1) / size);
+
+        return new MapSearchResult(result, items);
     }
 }
