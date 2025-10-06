5aaee72ce31647c5b622b13a1afc1a6cfb953808
chinhdang <dangchinh645@gmail.com>
2025-10-06 00:08:55 -0400

    add get media from cache or from database

--- src/main/java/dev/chinh/streamingservice/content/service/MediaService.java
+++ src/main/java/dev/chinh/streamingservice/content/service/MediaService.java
@@ -1,5 +1,9 @@
 package dev.chinh.streamingservice.content.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.chinh.streamingservice.data.MediaMetaDataRepository;
+import dev.chinh.streamingservice.data.entity.MediaMetaData;
+import dev.chinh.streamingservice.search.data.MediaSearchItem;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -8,4 +12,15 @@ public abstract class MediaService {
 
     protected final RedisTemplate<String, Object> redisTemplate;
     protected final MinIOService minIOService;
+    private final ObjectMapper objectMapper;
+    private final MediaMetaDataRepository mediaRepository;
+
+    protected MediaSearchItem getCachedMediaSearchItem(long id) {
+        return objectMapper.convertValue(redisTemplate.opsForValue().get("media::" + id), MediaSearchItem.class);
+    }
+
+    protected MediaMetaData findMediaMetaDataAllInfo(long id) {
+        return mediaRepository.findByIdWithAllInfo(id).orElseThrow(() ->
+                new IllegalArgumentException("Media not found with id " + id));
+    }
 }
