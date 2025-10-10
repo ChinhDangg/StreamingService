75f41c6b10e28ad06aceccbeab47b4581b28b3b0
Chinh Dang <dangchinh645@gmail.com>
2025-10-10 19:28:25 -0400

    add manual testing

--- src/main/java/dev/chinh/streamingservice/StreamingServiceApplication.java
+++ src/main/java/dev/chinh/streamingservice/StreamingServiceApplication.java
@@ -1,10 +1,17 @@
 package dev.chinh.streamingservice;
 
 import dev.chinh.streamingservice.content.service.MinIOService;
+import dev.chinh.streamingservice.search.service.OpenSearchService;
+import io.minio.Result;
+import io.minio.messages.Item;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 public class StreamingServiceApplication {
@@ -14,8 +21,19 @@ public class StreamingServiceApplication {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(MinIOService minIOService) {
+    public CommandLineRunner commandLineRunner(OpenSearchService openSearchService) {
         return args -> {
+            Map<String, Object> document = new HashMap<>();
+            document.put("title", "Test Video Sample 1");
+            document.put("universes", List.of("Nier Automata"));
+            document.put("bucket", "3dvid");
+            document.put("parentPath", "");
+            document.put("key", "2b.mp4");
+            document.put("length", 657);
+            document.put("width", 1920);
+            document.put("height", 1080);
+            document.put("id", 1);
+            //openSearchService.indexDocument(1, document);
         };
     }
 }
