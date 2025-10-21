837192e0baa66c96a4a7a912cbcfaa9394e32ba3
chinhdang <dangchinh645@gmail.com>
2025-10-21 18:23:23 -0400

    add another endpoint to cache last access album video

--- src/main/java/dev/chinh/streamingservice/InternalController.java
+++ src/main/java/dev/chinh/streamingservice/InternalController.java
@@ -1,7 +1,7 @@
 package dev.chinh.streamingservice;
 
 import dev.chinh.streamingservice.content.constant.Resolution;
-import dev.chinh.streamingservice.content.service.VideoService;
+import dev.chinh.streamingservice.content.service.AlbumService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,11 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("/internal/access")
 public class InternalController {
 
-    private final VideoService videoService;
+    private final AlbumService albumService;
 
     @PostMapping("/partial/{mediaId}/{resolution}")
     public void cacheMediaLastAccess(@PathVariable long mediaId, @PathVariable Resolution resolution) {
-        System.out.println(mediaId + "\t" + resolution);
-        videoService.addCacheLastAccess(videoService.getCacheMediaJobId(mediaId, resolution), null);
+        albumService.addCacheLastAccess(albumService.getCacheMediaJobId(mediaId, resolution), null);
+    }
+
+    @PostMapping("/album-partial/{albumId}/{vidNum}/{resolution}")
+    public void cacheMediaLastAccess(@PathVariable long albumId, @PathVariable int vidNum,
+                                     @PathVariable Resolution resolution) {
+        albumService.addCacheLastAccess(albumService.getAlbumVidCacheJobIdString(albumId, vidNum, resolution), null);
     }
 }
