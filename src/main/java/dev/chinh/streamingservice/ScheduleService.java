b58da2507cc48c16e7055aa088b87acd491b4a1d
chinhdang <dangchinh645@gmail.com>
2025-10-22 19:55:44 -0400

    add to schedule service to remove any non-accessing thumbnails

--- src/main/java/dev/chinh/streamingservice/ScheduleService.java
+++ src/main/java/dev/chinh/streamingservice/ScheduleService.java
@@ -2,11 +2,14 @@ package dev.chinh.streamingservice;
 
 import dev.chinh.streamingservice.content.constant.MediaJobStatus;
 import dev.chinh.streamingservice.content.service.VideoService;
+import dev.chinh.streamingservice.search.service.MediaSearchService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.Set;
 
 @Service
@@ -15,10 +18,13 @@ import java.util.Set;
 public class ScheduleService {
 
     private final VideoService videoService;
+    private final MediaSearchService mediaSearchService;
 
     @Scheduled(fixedRate = 60_000, initialDelay = 60_000)
     public void scheduled() throws Exception {
         stopNonViewingVideoRunningJob();
+        cleanThumbnails();
+        Thread.sleep(1_000);
         OSUtil.refreshUsableMemory();
     }
 
@@ -43,6 +49,24 @@ public class ScheduleService {
             String jobId = cachedJobStatus.get("jobId").toString(); // UUID
             videoService.stopFfmpegJob(jobId);
             videoService.addCacheTempVideoJobStatus(videoJobId, null, null, MediaJobStatus.STOPPED);
+        }
+    }
+
+    private void cleanThumbnails() throws IOException, InterruptedException {
+        System.out.println("cleaning thumbnails");
+        long now = System.currentTimeMillis();
+        Set<ZSetOperations.TypedTuple<Object>> lastAccessThumbnails = mediaSearchService.getAllThumbnailCacheLastAccess(now);
+        for (ZSetOperations.TypedTuple<Object> thumbnail : lastAccessThumbnails) {
+            long millisPassed = (long) (System.currentTimeMillis() - thumbnail.getScore());
+            if (millisPassed < 60_000) {
+                break;
+            }
+
+            String thumbnailFileName = (String) thumbnail.getValue();
+
+            String path = MediaSearchService.getThumbnailParentPath() + "/" + thumbnailFileName;
+            OSUtil.deleteForceMemoryDirectory(path);
+            mediaSearchService.removeThumbnailLastAccess(thumbnailFileName);
         }
     }
 }
