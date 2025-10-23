00f24698a4e629bed15937595bf4b6678f772793
Chinh Dang <dangchinh645@gmail.com>
2025-10-23 19:33:44 -0400

    add class to send info back to frontend

---
+++ src/main/java/dev/chinh/streamingservice/serve/MediaDisplayContent.java
@@ -0,0 +1,57 @@
+package dev.chinh.streamingservice.serve;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import dev.chinh.streamingservice.data.ContentMetaData;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MediaDisplayContent {
+
+    @JsonProperty(ContentMetaData.ID)
+    private Long id;
+
+    @JsonProperty(ContentMetaData.TITLE)
+    private String title;
+
+    @JsonProperty(ContentMetaData.THUMBNAIL)
+    private String thumbnail;
+
+    @JsonProperty(ContentMetaData.TAGS)
+    private List<String> tags;
+
+    @JsonProperty(ContentMetaData.CHARACTERS)
+    private List<String> characters;
+
+    @JsonProperty(ContentMetaData.UNIVERSES)
+    private List<String> universes;
+
+    @JsonProperty(ContentMetaData.AUTHORS)
+    private List<String> authors;
+
+    @JsonProperty(ContentMetaData.LENGTH)
+    private Integer length;
+
+    @JsonProperty(ContentMetaData.SIZE)
+    private Long size;
+
+    @JsonProperty(ContentMetaData.WIDTH)
+    private Integer width;
+
+    @JsonProperty(ContentMetaData.HEIGHT)
+    private Integer height;
+
+    @JsonProperty(ContentMetaData.UPLOAD_DATE)
+    private LocalDate uploadDate;
+
+    @JsonProperty(ContentMetaData.YEAR)
+    private Integer year;
+}
