f1b837fea4c63d9a4f22b8e13ee2a3aa257842ac
Chinh Dang <dangchinh645@gmail.com>
2025-10-23 19:34:14 -0400

    update the media group metadata

--- src/main/java/dev/chinh/streamingservice/data/entity/MediaGroupMetaData.java
+++ src/main/java/dev/chinh/streamingservice/data/entity/MediaGroupMetaData.java
@@ -15,8 +15,24 @@ public class MediaGroupMetaData {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // is group means not an individual item, that holds shared info and group other Media MetaData
-    private boolean isGroup;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "media_id")
+    private MediaMetaData mediaMetaData;
 
-    private Integer numInfo;    // episode/chapter for single media item or total episode/chapter for is group media
+    // Read-only access to foreign key column
+    @Column(name = "media_id", insertable = false, updatable = false)
+    private Long mediaMetaDataId;
+
+    // If has media group id then the media is an individual item that hold the grouper id.
+    // Media Grouper holds shared info and group other Media MetaData
+    // does not hold bucket, parentPath, and key info, and does not hold another media group metadata
+    // Individual media will share same grouper id to group together.
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "grouper_id")
+    private MediaGroupMetaData grouperMetaData;
+
+    @Column(name = "grouper_id", insertable = false, updatable = false)
+    private Long grouperMetaDataId;
+
+    private Integer numInfo;    // episode/chapter for single media item or total episode/chapter for is grouper media metadata
 }
