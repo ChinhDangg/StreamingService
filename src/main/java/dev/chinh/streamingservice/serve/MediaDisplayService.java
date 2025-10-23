92d53e4d6e8c0fe67865dc7dd0fcba4de0f6e2d6
Chinh Dang <dangchinh645@gmail.com>
2025-10-23 19:35:07 -0400

    add base method

--- src/main/java/dev/chinh/streamingservice/serve/MediaDisplayService.java
+++ src/main/java/dev/chinh/streamingservice/serve/MediaDisplayService.java
@@ -1,9 +1,26 @@
 package dev.chinh.streamingservice.serve;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.chinh.streamingservice.content.service.AlbumService;
+import dev.chinh.streamingservice.search.data.MediaSearchItem;
+import dev.chinh.streamingservice.search.service.MediaSearchService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MediaDisplayService {
 
-    
+    private final AlbumService albumService;
+    private final ObjectMapper objectMapper;
+
+    public MediaDisplayContent getMediaContentInfo(long mediaId) {
+        MediaSearchItem mediaItem = (MediaSearchItem) albumService.getMediaDescriptionGeneral(mediaId);
+
+        MediaDisplayContent mediaDisplayContent = objectMapper.convertValue(mediaItem, MediaDisplayContent.class);
+        if (mediaItem.hasThumbnail())
+            mediaDisplayContent.setThumbnail(MediaSearchService.getThumbnailPath(mediaId, MediaSearchService.thumbnailResolution, mediaItem.getThumbnail()));
+        return mediaDisplayContent;
+    }
+
 }
