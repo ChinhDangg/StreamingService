ee266f4a0789e3c59ba85770bc6d2fc25b03c26c
chinhdang <dangchinh645@gmail.com>
2025-10-28 12:49:22 -0400

    manual tests

--- backend/src/main/java/dev/chinh/streamingservice/StreamingServiceApplication.java
+++ backend/src/main/java/dev/chinh/streamingservice/StreamingServiceApplication.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @SpringBootApplication
@@ -25,30 +27,34 @@ public class StreamingServiceApplication {
         return args -> {
 //            Map<String, Object> document = new HashMap<>();
 //            document.put("title", "Test Video Sample 1");
-//            document.put("universes", List.of("Nier Automata"));
+//            document.put("universes", List.of(""));
 //            document.put("bucket", "3dvid");
 //            document.put("parentPath", "");
 //            document.put("key", "2b.mp4");
+//            document.put("uploadDate", LocalDate.now());
 //            document.put("length", 657);
 //            document.put("size", 400111222);
 //            document.put("width", 1920);
 //            document.put("height", 1080);
 //            document.put("id", 1);
-            //openSearchService.indexDocument(1, document);
+//            openSearchService.indexDocument(1, document);
             //openSearchService.deleteDocument(1);
 
 //            Map<String, Object> document = new HashMap<>();
 //            document.put("title", "Test Album");
-//            document.put("universes", List.of("Genshin Impact"));
+//            document.put("universes", List.of(""));
 //            document.put("bucket", "cos");
-//            document.put("parentPath", "aqua/Legion/Aqua水淼Clorinde");
+//            document.put("parentPath", "");
+              // document.put("thumbnail", "");
+//            document.put("uploadDate", LocalDate.now());
+//            document.put("year", 2025);
 //            document.put("length", 92);
 //            document.put("size", 180111222);
 //            document.put("id", 2);
 //            openSearchService.indexDocument(2, document);
 
             //openSearchService.partialUpdateDocument(1, Map.of("width", 3840, "height", 2160));
-            //openSearchService.partialUpdateDocument(2, Map.of("uploadDate", LocalDate.now()));
+            //openSearchService.partialUpdateDocument(2, Map.of("thumbnail", ""));
             //openSearchService.createIndexWithMapping();
         };
     }
