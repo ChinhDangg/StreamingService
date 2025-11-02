c5217c4a8ec995c664270bd4a25b5f00684d6340
chinh <dangchinh645@gmail.com>
2025-11-01 23:05:06 -0400

    add endpoint for getting video page fragment and album page fragment

--- frontend/src/main/java/dev/chinh/streamingservice/FragmentContentController.java
+++ frontend/src/main/java/dev/chinh/streamingservice/FragmentContentController.java
@@ -16,12 +16,10 @@ public class FragmentContentController {
 
     private final TemplateEngine templateEngine;
 
-    @GetMapping("/video")
+    @GetMapping("/video-player")
     public Map<String, Object> getCombinedContent() {
-
         Context context = new Context();
 
-        // 1. Define the template file and the list of fragment names
         String templateName = "video-player/video-player";
 
         Map<String, String> fragmentMap = new HashMap<>();
@@ -29,22 +27,57 @@ public class FragmentContentController {
         fragmentMap.put("html", "video-player-html");
         fragmentMap.put("script", "video-player-script");
 
+        return getCombinedFragmentAsResponse(context, templateName, fragmentMap);
+    }
+
+    @GetMapping("/video")
+    public Map<String, Object> getVideoContent() {
+        Context context = new Context();
+
+        String templateName = "video-player/video-page";
+
+        Map<String, String> fragmentMap = new HashMap<>();
+        fragmentMap.put("style", "video-page-style");
+        fragmentMap.put("html", "video-page-html");
+        fragmentMap.put("script", "video-page-script");
+
+        return getCombinedFragmentAsResponse(context, templateName, fragmentMap);
+    }
+
+    @GetMapping("/album")
+    public Map<String, Object> getAlbumContent() {
+        Context context = new Context();
+
+        String templateName = "video-player/album-page";
+
+        Map<String, String> fragmentMap = new HashMap<>();
+        fragmentMap.put("style", "album-style");
+        fragmentMap.put("html", "album-html");
+        fragmentMap.put("script", "album-script");
+
+        return getCombinedFragmentAsResponse(context, templateName, fragmentMap);
+    }
+
+    private Map<String, Object> getCombinedFragmentAsResponse(Context context, String templateName,
+                                                              Map<String, String> fragmentMap) {
         Map<String, Object> response = new HashMap<>();
 
         for (Map.Entry<String, String> entry : fragmentMap.entrySet()) {
             Set<String> selectors = Collections.singleton(entry.getValue());
             String fragmentHtml = templateEngine.process(templateName, selectors, context);
             response.put(entry.getKey(), fragmentHtml);
+            System.out.println(fragmentHtml);
         }
 
-        String scripts = response.get("script").toString();
-        String[] parts = scripts.split("\n");
-        String[] scriptOnly = Arrays.stream(parts)
-                .skip(1)
-                .limit(parts.length - 2)
-                .map(String::trim)
-                .toArray(String[]::new);
-        response.put("script", scriptOnly);
+        if (response.get("script") != null) {
+            String scripts = response.get("script").toString();
+            String[] parts = scripts.split("\n");
+            String[] scriptOnly = Arrays.stream(parts)
+                    .map(String::trim)
+                    .filter(s -> !s.isBlank())
+                    .toArray(String[]::new);
+            response.put("script", scriptOnly);
+        }
 
         return response;
     }
