805cb7fcc45482bd7446f4ee85c3001c093ebab4
chinh <dangchinh645@gmail.com>
2025-11-08 16:01:02 -0500

    add live total time for live transcoding video

--- frontend/src/main/resources/static/js/set-video-url.js
+++ frontend/src/main/resources/static/js/set-video-url.js
@@ -1,6 +1,7 @@
 
 export function setVideoUrl(videoContainerNode, playlistUrl = "p720/master.m3u8") {
     const video = videoContainerNode.querySelector('video');
+    const totalTime = videoContainerNode.querySelector('.total-time');
     if (playlistUrl.endsWith(".m3u8")) {
         if (video.canPlayType('application/vnd.apple.mpegurl')) {
             video.src = playlistUrl + '?_=' + Date.now(); // cache-buster
@@ -8,8 +9,24 @@ export function setVideoUrl(videoContainerNode, playlistUrl = "p720/master.m3u8"
             const hls = new Hls({ startPosition: 0 });
             hls.loadSource(playlistUrl + '?_=' + Date.now());
             hls.attachMedia(video);
+            const onLevelLoaded = (event, data) => {
+                const d = data.details;
+                console.log('HLS details:', d);
+                console.log('live: ', d.live);
+                if (!d.live) {
+                    hls.off(Hls.Events.LEVEL_LOADED, onLevelLoaded);
+                    totalTime.textContent = formatTime(video.duration);
+                } else {
+                    setTimeout(() => {
+                        totalTime.textContent = formatTime(video.duration);
+                    }, 100);
+                }
+            }
+            hls.on(Hls.Events.LEVEL_LOADED, onLevelLoaded);
         }
     } else {
         video.src = playlistUrl;
     }
 }
+
+const formatTime = s => `${Math.floor(s / 60)}:${Math.floor(s % 60).toString().padStart(2, '0')}`;
