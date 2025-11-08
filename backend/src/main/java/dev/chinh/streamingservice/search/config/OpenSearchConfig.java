75e282236a12ed5f8ee020f4d2ffa5b3b06b023f
chinh <dangchinh645@gmail.com>
2025-11-08 13:01:31 -0500

    replace old rest high level client with opensearch-java client

--- backend/src/main/java/dev/chinh/streamingservice/search/config/OpenSearchConfig.java
+++ backend/src/main/java/dev/chinh/streamingservice/search/config/OpenSearchConfig.java
@@ -5,22 +5,39 @@ import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.hc.client5.http.auth.AuthScope;
+import org.apache.hc.client5.http.auth.UsernamePasswordCredentials;
+import org.apache.hc.client5.http.impl.auth.BasicCredentialsProvider;
 import org.apache.hc.core5.http.HttpHost;
-import org.opensearch.client.RestClient;
-import org.opensearch.client.RestHighLevelClient;
+import org.opensearch.client.opensearch.OpenSearchClient;
+import org.opensearch.client.transport.OpenSearchTransport;
+import org.opensearch.client.transport.httpclient5.ApacheHttpClient5TransportBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.net.URISyntaxException;
 
 @Configuration
 public class OpenSearchConfig {
 
-    @Bean(destroyMethod = "close")
-    public static RestHighLevelClient openSearchClient() throws URISyntaxException {
-        return new RestHighLevelClient(
-                RestClient.builder(HttpHost.create("http://localhost:9200"))
+    @Bean
+    public OpenSearchClient createClient() {
+
+        HttpHost host = new HttpHost("http", "localhost", 9200);
+
+        BasicCredentialsProvider credsProvider = new BasicCredentialsProvider();
+        credsProvider.setCredentials(
+                new AuthScope(host),
+                new UsernamePasswordCredentials("username", "password".toCharArray())
         );
+
+        // Create the low-level Apache HTTP client transport
+        OpenSearchTransport transport = ApacheHttpClient5TransportBuilder
+                .builder(host)
+//                .setHttpClientConfigCallback(httpClientBuilder ->
+//                        httpClientBuilder.setDefaultCredentialsProvider(credsProvider)
+//                )
+                .build();
+
+        return new OpenSearchClient(transport);
     }
 
     @Bean
