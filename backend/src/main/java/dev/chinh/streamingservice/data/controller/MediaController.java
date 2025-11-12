f332a930f9171bf6b746701ab27df6cb5b26d656
chinh <dangchinh645@gmail.com>
2025-11-12 13:21:35 -0500

    add media controller to browse all name entries by category

---
+++ backend/src/main/java/dev/chinh/streamingservice/data/controller/MediaController.java
@@ -0,0 +1,45 @@
+package dev.chinh.streamingservice.data.controller;
+
+import dev.chinh.streamingservice.data.service.MediaMetadataService;
+import dev.chinh.streamingservice.search.constant.SortBy;
+import lombok.RequiredArgsConstructor;
+import org.opensearch.client.opensearch._types.SortOrder;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/media")
+public class MediaController {
+
+    private final MediaMetadataService mediaMetadataService;
+
+    @GetMapping("/authors")
+    public ResponseEntity<?> getAuthors(@RequestParam(value = "p", defaultValue = "0") int offset,
+                                        @RequestParam(value = "by", defaultValue = "NAME") SortBy sortBy,
+                                        @RequestParam(value = "order", defaultValue = "ASC") Sort.Direction order) {
+        return ResponseEntity.ok().body(mediaMetadataService.findAllAuthors(offset, sortBy, order));
+    }
+
+    @GetMapping("/characters")
+    public ResponseEntity<?> getCharacters(@RequestParam(value = "p", defaultValue = "0") int offset,
+                                           @RequestParam(value = "by", defaultValue = "NAME") SortBy sortBy,
+                                            @RequestParam(value = "order", defaultValue = "ASC") Sort.Direction order) {
+        return ResponseEntity.ok().body(mediaMetadataService.findAllCharacters(offset, sortBy, order));
+    }
+
+    @GetMapping("/universes")
+    public ResponseEntity<?> getUniverses(@RequestParam(value = "p", defaultValue = "0") int offset,
+                                          @RequestParam(value = "by", defaultValue = "NAME") SortBy sortBy,
+                                            @RequestParam(value = "order", defaultValue = "ASC") Sort.Direction order) {
+        return ResponseEntity.ok().body(mediaMetadataService.findAllUniverses(offset, sortBy, order));
+    }
+
+    @GetMapping("/tags")
+    public ResponseEntity<?> getTags(@RequestParam(value = "p", defaultValue = "0") int offset,
+                                     @RequestParam(value = "by", defaultValue = "NAME") SortBy sortBy,
+                                     @RequestParam(value = "order", defaultValue = "ASC") Sort.Direction order) {
+        return ResponseEntity.ok().body(mediaMetadataService.findAllTags(offset, sortBy, order));
+    }
+}
