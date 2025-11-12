ad74a876d66b01b34916654cf281540f3ce8119d
chinh <dangchinh645@gmail.com>
2025-11-12 13:22:01 -0500

    add media name entry as dto

---
+++ backend/src/main/java/dev/chinh/streamingservice/data/dto/MediaNameEntry.java
@@ -0,0 +1,27 @@
+package dev.chinh.streamingservice.data.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import dev.chinh.streamingservice.data.ContentMetaData;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MediaNameEntry {
+
+    @JsonProperty(ContentMetaData.NAME)
+    private String name;
+
+    @JsonProperty(ContentMetaData.LENGTH)
+    private int length;
+
+    @JsonProperty(ContentMetaData.UPLOAD_DATE)
+    private LocalDate uploadDate;
+
+    @JsonProperty(ContentMetaData.THUMBNAIL)
+    private String thumbnail;
+}
