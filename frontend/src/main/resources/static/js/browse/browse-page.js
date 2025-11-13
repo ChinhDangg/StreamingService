fa0dfaad5dd0261fd1c6e6f4f827fdf80b5b230c
chinh <dangchinh645@gmail.com>
2025-11-12 19:59:00 -0500

    add browse page js

---
+++ frontend/src/main/resources/static/js/browse/browse-page.js
@@ -0,0 +1,235 @@
+import { displayPagination } from "/static/js/pagination.js";
+
+const NameEntry = Object.freeze({
+    Characters: 'characters',
+    Universes: 'universes',
+    Authors: 'authors',
+    Tags: 'tags',
+});
+
+const SortBy = Object.freeze({
+    Name: 'NAME',
+    Upload: 'UPLOAD_DATE',
+    Total: 'LENGTH',
+});
+
+const SortOrder = Object.freeze({
+    Ascending: 'ASC',
+    Descending: 'DESC',
+});
+
+let currentNameEntry = getCurrentNameEntry();
+let currentPage = 0;
+let currentSortBy = SortBy.Name;
+let currentSortOrder = SortOrder.Ascending;
+
+let previousSortByButton = null;
+
+function getCurrentNameEntry(url = window.location.href) {
+    const pathName = new URL(url).pathname;
+    for (const [key, value] of Object.entries(NameEntry)) {
+        if (pathName.endsWith(value)) {
+            return key;
+        }
+    }
+    return Object.keys(NameEntry)[0];
+}
+
+async function initialize() {
+    initializeCurrentOptions();
+
+    initializeNameBrowseOptions();
+    removeRedirectBrowseOptions();
+    initializeSortByOptions();
+    initializeSortByOrderOptions();
+
+    await fetchNameItems(currentNameEntry, currentPage, currentSortBy, currentSortOrder);
+}
+
+initialize();
+
+function initializeCurrentOptions() {
+    const urlParams = new URLSearchParams(window.location.search);
+    currentPage = Number(urlParams.get('p')) || 0;
+    currentSortBy = urlParams.get('by') || SortBy.Name;
+    currentSortOrder = urlParams.get('order') || SortOrder.Ascending;
+}
+
+function initializeNameBrowseOptions() {
+    const browseOptionContainer = document.getElementById('browse-option-container');
+    const first = browseOptionContainer.firstElementChild;
+    if (first) browseOptionContainer.replaceChildren(first);
+    for (const [key, value] of Object.entries(NameEntry)) {
+        const browseOption = helperCloneAndUnHideNode(first);
+        browseOption.href = `/page/browse/${value}`;
+        browseOption.textContent = key;
+        browseOptionContainer.appendChild(browseOption);
+    }
+}
+
+function removeRedirectBrowseOptions () {
+    const browseOptionContainer = document.getElementById('browse-option-container');
+    browseOptionContainer.querySelectorAll('a').forEach(browseOption => {
+        if (browseOption.href) {
+            const nameEntry = getCurrentNameEntry(browseOption.href);
+            browseOption.addEventListener('click', async (e) => {
+                e.preventDefault();
+                if (currentNameEntry === nameEntry) return;
+                currentNameEntry = nameEntry;
+                await fetchNameItems(NameEntry[currentNameEntry], currentPage, currentSortBy, currentSortOrder);
+            });
+        }
+    });
+}
+
+function initializeSortByOptions() {
+    const sortByContainer = document.getElementById('sort-by-option-container');
+    const optionBtnTem = sortByContainer.querySelector('button');
+    for (const [key, value] of Object.entries(SortBy)) {
+        const optionBtn = helperCloneAndUnHideNode(optionBtnTem);
+        if (currentSortBy === value) {
+            optionBtn.classList.add('bg-indigo-600');
+            optionBtn.classList.remove('bg-gray-800');
+            previousSortByButton = optionBtn;
+        }
+        optionBtn.textContent = key;
+        optionBtn.addEventListener('click', async () => {
+            if (previousSortByButton === optionBtn) return;
+            currentSortBy = value;
+            previousSortByButton?.classList.remove('bg-indigo-600');
+            previousSortByButton?.classList.add('bg-gray-800');
+            optionBtn.classList.add('bg-indigo-600');
+            optionBtn.classList.remove('bg-gray-800');
+            previousSortByButton = optionBtn;
+            await fetchNameItems(currentNameEntry, currentPage, currentSortBy, currentSortOrder);
+        });
+        sortByContainer.appendChild(optionBtn);
+    }
+}
+
+function initializeSortByOrderOptions() {
+    const sortOrderContainer = document.getElementById('sort-order-option-container');
+    const optionBtn = sortOrderContainer.querySelector('button');
+    optionBtn.textContent = getSortOrderText(currentSortOrder);
+    optionBtn.addEventListener('click', async () => {
+        currentSortOrder = currentSortOrder === SortOrder.Ascending ? SortOrder.Descending : SortOrder.Ascending;
+        optionBtn.textContent = getSortOrderText(currentSortOrder);
+        await fetchNameItems(currentNameEntry, currentPage, currentSortBy, currentSortOrder);
+    });
+}
+
+function getSortOrderText(order) {
+    for (const [key, value] of Object.entries(SortOrder)) {
+        if (value === order) return key;
+    }
+}
+
+async function fetchNameItems(nameEntry, p, by, order) {
+    const urlParams = new URLSearchParams({ p, by, order });
+    const response = fetch(`/api/media/${nameEntry}?${urlParams}`);
+    if (!response.ok) {
+        alert("Failed to fetch name items");
+        return;
+    }
+    const nameItems = response.json();
+
+    const url = new URL(window.location.href);
+    const pageBrowseUrl = getBrowsePageUrl(currentPage);
+    const newUrl = url.origin + pageBrowseUrl;
+    window.history.pushState({ path: newUrl }, '', newUrl);
+    // const nameItems = {
+    //     "content": [
+    //         {
+    //             "name": "Kafka",
+    //             "length": 60,
+    //             "uploadDate": "2025-11-04",
+    //             "thumbnail": "https://placehold.co/1080x1920"
+    //         },
+    //         {
+    //             "name": "Kafka",
+    //             "length": 60,
+    //             "uploadDate": "2025-11-04",
+    //             "thumbnail": "https://placehold.co/1920x1080"
+    //         }
+    //     ],
+    //     "pageable": {
+    //         "pageNumber": 0,
+    //         "pageSize": 20,
+    //         "sort": {
+    //             "orders": []
+    //         }
+    //     },
+    //     "total": 1
+    // };
+
+    await displayItem(nameItems);
+    displayPagination(nameItems.pageable.pageNumber,
+        Math.trunc((nameItems.total + nameItems.pageable.pageSize - 1) / nameItems.pageable.pageSize),
+        getBrowsePageUrl, pageClickHandler);
+}
+
+async function displayItem(nameItems) {
+    const browseTitle = document.getElementById('browse-page-title');
+    browseTitle.querySelector('.title').textContent = currentNameEntry;
+    browseTitle.querySelector('.total').textContent = nameItems.total;
+
+    const hasThumbnail = true; //NameEntry[currentNameEntry] === nameEntry.Characters || NameEntry[currentNameEntry] === nameEntry.Universes;
+    const nameContainer = hasThumbnail ? document.getElementById('main-item-container')
+        : document.getElementById('main-text-item-container');
+
+    nameContainer.innerHTML = '';
+    nameContainer.classList.remove('hidden');
+
+    if (nameItems.total === 0) {
+        nameContainer.innerText = 'No items found';
+        return;
+    }
+
+    const browseContainer = document.getElementById('browse-item-container');
+
+    const loadImage = (url) => new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => resolve(img);
+        img.onerror = (e) => reject(new Error(`Failed to load image: ${url}`, { cause: e }));
+        img.src = url;
+    });
+
+    for (const item of nameItems.content) {
+        let itemNode;
+        if (hasThumbnail) {
+            const loadedImage = await loadImage(item.thumbnail);
+            const horizontal = loadedImage.naturalWidth >= loadedImage.naturalHeight;
+            const itemNodeTem = horizontal ? browseContainer.querySelector('.horizontal-item') : browseContainer.querySelector('.vertical-item');
+            itemNode = helperCloneAndUnHideNode(itemNodeTem);
+            itemNode.querySelector('img').src = item.thumbnail;
+            loadedImage.remove();
+        } else {
+            itemNode = helperCloneAndUnHideNode(browseContainer.querySelector('.text-item'));
+        }
+        itemNode.querySelector('.name-title').textContent = item.name;
+        itemNode.querySelector('.date-note').textContent = item.uploadDate;
+        itemNode.querySelector('.total-note').textContent = item.length;
+        itemNode.querySelector('.item-link').href = `/page/search?${NameEntry[currentNameEntry]}=${item.name}`;
+        nameContainer.appendChild(itemNode);
+    }
+}
+
+function getBrowsePageUrl(page) {
+    const urlParams = new URLSearchParams({ p: page, by: currentSortBy, order: currentSortOrder});
+    return `/page/browse/${NameEntry[currentNameEntry]}?${urlParams}`;
+}
+
+async function pageClickHandler(e, page) {
+    e.preventDefault();
+    currentPage = page;
+    e.target.disabled = true;
+    fetchNameItems().then(() => {
+        e.target.disabled = false;
+    });
+}
+
+function helperCloneAndUnHideNode(node) {
+    const clone = node.cloneNode(true);
+    clone.classList.remove('hidden');
+    return clone;
+}
