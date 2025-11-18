72f72d10964ea8f2412b8c74a0e5c5db162c5f5a
chinh <dangchinh645@gmail.com>
2025-11-17 22:46:17 -0500

    remove thumbnail generation from search service

--- backend/src/main/java/dev/chinh/streamingservice/search/service/MediaSearchService.java
+++ backend/src/main/java/dev/chinh/streamingservice/search/service/MediaSearchService.java
@@ -3,11 +3,9 @@ package dev.chinh.streamingservice.search.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.chinh.streamingservice.MediaMapper;
 import dev.chinh.streamingservice.content.constant.MediaType;
-import dev.chinh.streamingservice.content.constant.Resolution;
-import dev.chinh.streamingservice.content.service.AlbumService;
 import dev.chinh.streamingservice.data.ContentMetaData;
-import dev.chinh.streamingservice.data.entity.MediaDescription;
 import dev.chinh.streamingservice.data.service.MediaMetadataService;
+import dev.chinh.streamingservice.data.service.ThumbnailService;
 import dev.chinh.streamingservice.search.constant.SortBy;
 import dev.chinh.streamingservice.search.data.*;
 import lombok.RequiredArgsConstructor;
@@ -15,12 +13,9 @@ import org.apache.coyote.BadRequestException;
 import org.opensearch.client.opensearch._types.SortOrder;
 import org.opensearch.client.opensearch.core.SearchResponse;
 import org.opensearch.client.opensearch.core.search.Hit;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.*;
 
@@ -28,14 +23,11 @@ import java.util.*;
 @RequiredArgsConstructor
 public class MediaSearchService {
 
-    private final RedisTemplate<String, Object> redisTemplate;
     private final ObjectMapper mapper;
     private final MediaMapper mediaMapper;
     private final OpenSearchService openSearchService;
-    private final AlbumService albumService;
     private final MediaMetadataService mediaMetadataService;
-
-    public static final Resolution thumbnailResolution = Resolution.p360;
+    private final ThumbnailService thumbnailService;
 
     record NameEntry(String name) {}
 
@@ -99,7 +91,7 @@ public class MediaSearchService {
         MapSearchResult mapSearchResult = mapResponseToMediaSearchResult(
                 openSearchService.advanceSearch(includes, excludes, request.getRangeFields(), page, size, sortBy, sortOrder), page, size);
 
-        processThumbnails(mapSearchResult.searchItems);
+        thumbnailService.processThumbnails(mapSearchResult.searchItems);
         cacheMediaSearchItems(mapSearchResult.searchItems);
         return mapSearchResult.searchResult;
     }
@@ -130,7 +122,7 @@ public class MediaSearchService {
         MapSearchResult mapSearchResult = mapResponseToMediaSearchResult(
                 openSearchService.search(searchString, page, size, sortBy, sortOrder), page, size);
 
-        processThumbnails(mapSearchResult.searchItems);
+        thumbnailService.processThumbnails(mapSearchResult.searchItems);
         cacheMediaSearchItems(mapSearchResult.searchItems);
         return mapSearchResult.searchResult;
     }
@@ -141,7 +133,7 @@ public class MediaSearchService {
         MapSearchResult mapSearchResult = mapResponseToMediaSearchResult(
                 openSearchService.searchTermByOneField(field, keywords, page, size, sortBy, sortOrder), page, size);
 
-        processThumbnails(mapSearchResult.searchItems);
+        thumbnailService.processThumbnails(mapSearchResult.searchItems);
         cacheMediaSearchItems(mapSearchResult.searchItems);
         return mapSearchResult.searchResult;
     }
@@ -150,73 +142,9 @@ public class MediaSearchService {
         MapSearchResult mapSearchResult = mapResponseToMediaSearchResult(
                 openSearchService.searchMatchAll(page, size, sortBy, sortOrder), page, size);
 
-        processThumbnails(mapSearchResult.searchItems);
+        thumbnailService.processThumbnails(mapSearchResult.searchItems);
         cacheMediaSearchItems(mapSearchResult.searchItems);
         return mapSearchResult.searchResult;
-    }
-
-    private void processThumbnails(Collection<MediaSearchItem> items) {
-        long now = System.currentTimeMillis() + 60 * 60 * 1000;
-        List<MediaDescription> newThumbnails = new ArrayList<>();
-        for (MediaDescription item : items) {
-            if (!item.hasThumbnail())
-                continue;
-            Boolean addedCache = addCacheThumbnails(
-                    Paths.get(getThumbnailPath(item.getId(), thumbnailResolution, item.getThumbnail())).getFileName().toString(), now);
-            if (addedCache) { // if new thumbnail (not in cache)
-                newThumbnails.add(item);
-            }
-        }
-        if (newThumbnails.isEmpty())
-            return;
-        var albumUrlInfo = getMixThumbnailImagesAsAlbumUrls(newThumbnails, thumbnailResolution);
-        try {
-            if (albumUrlInfo.mediaUrlList().isEmpty())
-                return;
-            albumService.processResizedImagesInBatch(albumUrlInfo, thumbnailResolution, getThumbnailParentPath(), false);
-        } catch (InterruptedException | IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public AlbumService.AlbumUrlInfo getMixThumbnailImagesAsAlbumUrls(List<MediaDescription> mediaDescriptionList, Resolution resolution) {
-        List<String> pathList = new ArrayList<>();
-        List<AlbumService.MediaUrl> albumUrlList = new ArrayList<>();
-        List<String> bucketList = new ArrayList<>();
-        for (MediaDescription mediaDescription : mediaDescriptionList) {
-            if (!mediaDescription.hasThumbnail())
-                continue;
-
-            bucketList.add(mediaDescription.getBucket());
-            pathList.add(mediaDescription.getThumbnail());
-
-            String pathString = "/chunks" + getThumbnailPath(mediaDescription.getId(), resolution, mediaDescription.getThumbnail());
-            albumUrlList.add(new AlbumService.MediaUrl(MediaType.IMAGE, pathString));
-        }
-        return new AlbumService.AlbumUrlInfo(albumUrlList, bucketList, pathList);
-    }
-
-    private Boolean addCacheThumbnails(String thumbnailFileName, long expiry) {
-        return redisTemplate.opsForZSet().add("thumbnail-cache", thumbnailFileName, expiry);
-    }
-
-    public Set<ZSetOperations.TypedTuple<Object>> getAllThumbnailCacheLastAccess(long max) {
-        return redisTemplate.opsForZSet()
-                .rangeByScoreWithScores("thumbnail-cache", 0, max, 0, 50);
-    }
-
-    public void removeThumbnailLastAccess(String thumbnailFileName) {
-        redisTemplate.opsForZSet().remove("thumbnail-cache", thumbnailFileName);
-    }
-
-    public static String getThumbnailPath(long albumId, Resolution resolution, String thumbnail) {
-        String originalExtension = thumbnail.contains(".") ? thumbnail.substring(thumbnail.lastIndexOf(".") + 1)
-                .toLowerCase() : "jpg";
-        return getThumbnailParentPath() + "/" + albumId + "_" + resolution + "." + originalExtension;
-    }
-
-    public static String getThumbnailParentPath() {
-        return "/thumbnail-cache/" + thumbnailResolution;
     }
 
     public record MapSearchResult(
@@ -228,11 +156,11 @@ public class MediaSearchService {
         List<MediaSearchItem> items = new ArrayList<>();
         List<MediaSearchItemResponse> itemResponses = new ArrayList<>();
         for (Hit<Object> hit : response.hits().hits()) {
-            MediaSearchItem searchItem = mapper.convertValue(hit.source(),  MediaSearchItem.class);
+            MediaSearchItem searchItem = mapper.convertValue(hit.source(), MediaSearchItem.class);
             items.add(searchItem);
             MediaSearchItemResponse itemResponse = mediaMapper.map(searchItem);
-            itemResponse.setThumbnail(searchItem.hasThumbnail() ? getThumbnailPath(
-                    searchItem.getId(), thumbnailResolution, searchItem.getThumbnail()) : null);
+            itemResponse.setThumbnail(searchItem.hasThumbnail() ? ThumbnailService.getThumbnailPath(
+                    searchItem.getId(), searchItem.getThumbnail()) : null);
             itemResponse.setMediaType(searchItem.hasKey() ? MediaType.VIDEO : MediaType.IMAGE);
             itemResponses.add(itemResponse);
         }
