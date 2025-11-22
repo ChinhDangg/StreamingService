18eb839ebe64bce8d95854638f24356054a9630c
chinh <dangchinh645@gmail.com>
2025-11-21 20:58:13 -0500

    add request as separate class for getter and setter

---
+++ backend/src/main/java/dev/chinh/streamingservice/upload/NameAndThumbnailPostRequest.java
@@ -0,0 +1,18 @@
+package dev.chinh.streamingservice.upload;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NameAndThumbnailPostRequest {
+    @NotBlank
+    private String name;
+    @NotNull
+    private MultipartFile thumbnail;
+}
