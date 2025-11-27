3bbae1774384c66744dcd6208410e08160e10edc
chinh <dangchinh645@gmail.com>
2025-11-27 00:42:51 -0500

    add service to update media name entity info

---
+++ backend/src/main/java/dev/chinh/streamingservice/upload/MediaMetadataModifyService.java
@@ -0,0 +1,66 @@
+package dev.chinh.streamingservice.upload;
+
+import dev.chinh.streamingservice.data.ContentMetaData;
+import dev.chinh.streamingservice.data.repository.MediaMetaDataRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MediaMetadataModifyService {
+
+    private final MediaMetaDataRepository mediaMetaDataRepository;
+
+    public List<NameEntityDTO> getMediaAuthorInfo(long mediaId) {
+        return mediaMetaDataRepository.findAuthorsByMediaId(mediaId);
+    }
+
+    public List<NameEntityDTO> getMediaCharacterInfo(long mediaId) {
+        return mediaMetaDataRepository.findCharactersByMediaId(mediaId);
+    }
+
+    public List<NameEntityDTO> getMediaUniverseInfo(long mediaId) {
+        return mediaMetaDataRepository.findUniversesByMediaId(mediaId);
+    }
+
+    public List<NameEntityDTO> getMediaTagInfo(long mediaId) {
+        return mediaMetaDataRepository.findTagsByMediaId(mediaId);
+    }
+
+    public record UpdateList(
+            List<NameEntityDTO> added, List<NameEntityDTO> removed, String nameEntity
+    ) {}
+
+    @Transactional
+    public void updateNameEntityInMedia(UpdateList updateList, long mediaId) {
+        for (NameEntityDTO nameEntityDTO : updateList.removed) {
+            removeNameEntityInMedia(mediaId, nameEntityDTO.getId(), updateList.nameEntity);
+        }
+        for (NameEntityDTO nameEntityDTO : updateList.added) {
+            addNameEntityInMedia(mediaId, nameEntityDTO.getId(), updateList.nameEntity);
+        }
+    }
+
+    private void removeNameEntityInMedia(long mediaId, long nameEntityId, String nameEntity) {
+        switch (nameEntity) {
+            case ContentMetaData.AUTHORS -> mediaMetaDataRepository.deleteAuthorFromMedia(mediaId, nameEntityId);
+            case ContentMetaData.CHARACTERS -> mediaMetaDataRepository.deleteCharacterFromMedia(mediaId, nameEntityId);
+            case ContentMetaData.UNIVERSES -> mediaMetaDataRepository.deleteUniverseFromMedia(mediaId, nameEntityId);
+            case ContentMetaData.TAGS -> mediaMetaDataRepository.deleteTagFromMedia(mediaId, nameEntityId);
+            default -> throw new RuntimeException("Invalid name entity type: " + nameEntity);
+        }
+    }
+
+    private void addNameEntityInMedia(long mediaId, long nameEntityId, String nameEntity) {
+        switch (nameEntity) {
+            case ContentMetaData.AUTHORS -> mediaMetaDataRepository.addAuthorToMedia(mediaId, nameEntityId);
+            case ContentMetaData.CHARACTERS -> mediaMetaDataRepository.addCharacterToMedia(mediaId, nameEntityId);
+            case ContentMetaData.UNIVERSES -> mediaMetaDataRepository.addUniverseToMedia(mediaId, nameEntityId);
+            case ContentMetaData.TAGS -> mediaMetaDataRepository.addTagToMedia(mediaId, nameEntityId);
+            default -> throw new RuntimeException("Invalid name entity type: " + nameEntity);
+        }
+    }
+}
