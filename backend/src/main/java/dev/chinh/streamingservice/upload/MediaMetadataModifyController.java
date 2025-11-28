108abf4dddfbb2754281e220e28ddb65878eeda6
chinh <dangchinh645@gmail.com>
2025-11-28 13:14:11 -0500

    collapse getting media name entity info endpoints into one

--- backend/src/main/java/dev/chinh/streamingservice/upload/MediaMetadataModifyController.java
+++ backend/src/main/java/dev/chinh/streamingservice/upload/MediaMetadataModifyController.java
@@ -13,24 +13,10 @@ public class MediaMetadataModifyController {
 
     private final MediaMetadataModifyService mediaMetadataModifyService;
 
-    @GetMapping("/authors/{id}")
-    public ResponseEntity<List<NameEntityDTO>> getMediaAuthorInfo(@PathVariable long id) {
-        return ResponseEntity.ok().body(mediaMetadataModifyService.getMediaNameEntityInfo(id, MediaNameEntityConstant.AUTHORS));
-    }
-
-    @GetMapping("/characters/{id}")
-    public ResponseEntity<List<NameEntityDTO>> getMediaCharacterInfo(@PathVariable long id) {
-        return ResponseEntity.ok().body(mediaMetadataModifyService.getMediaNameEntityInfo(id, MediaNameEntityConstant.CHARACTERS));
-    }
-
-    @GetMapping("/universes/{id}")
-    public ResponseEntity<List<NameEntityDTO>> getMediaUniverseInfo(@PathVariable long id) {
-        return ResponseEntity.ok().body(mediaMetadataModifyService.getMediaNameEntityInfo(id, MediaNameEntityConstant.UNIVERSES));
-    }
-
-    @GetMapping("/tags/{id}")
-    public ResponseEntity<List<NameEntityDTO>> getMediaTagInfo(@PathVariable long id) {
-        return ResponseEntity.ok().body(mediaMetadataModifyService.getMediaNameEntityInfo(id, MediaNameEntityConstant.TAGS));
+    @GetMapping("/{nameEntity}/{id}")
+    public ResponseEntity<List<NameEntityDTO>> getMediaNameEntityInfo(@PathVariable MediaNameEntityConstant nameEntity,
+                                                                      @PathVariable long id) {
+        return ResponseEntity.ok().body(mediaMetadataModifyService.getMediaNameEntityInfo(id, nameEntity));
     }
 
     @PutMapping("/update/{id}")
