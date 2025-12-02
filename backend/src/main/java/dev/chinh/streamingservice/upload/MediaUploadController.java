c140d49be6f0b780289e1e17d83c21711eac232b
chinh <dangchinh645@gmail.com>
2025-12-01 21:47:30 -0500

    add media upload controller

---
+++ backend/src/main/java/dev/chinh/streamingservice/upload/MediaUploadController.java
@@ -0,0 +1,72 @@
+package dev.chinh.streamingservice.upload;
+
+import dev.chinh.streamingservice.content.constant.MediaType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/upload/media")
+public class MediaUploadController {
+
+    private final MediaUploadService mediaUploadService;
+
+    public record InitiateMultipartUploadRequest(String sessionId, String objectKey, MediaType mediaType) {}
+
+    @PostMapping("/create-session")
+    public String initiateSession(@RequestBody InitiateMultipartUploadRequest request) {
+        return mediaUploadService.initiateMediaUploadRequest(request.objectKey, request.mediaType);
+    }
+
+    @PostMapping("/initiate")
+    public String initiateUpload(@RequestBody InitiateMultipartUploadRequest request) {
+        return mediaUploadService.initiateMultipartUploadRequest(request.sessionId, request.objectKey, request.mediaType);
+    }
+
+    public record PresignUploadRequest(
+            String objectKey,
+            String uploadId,
+            int partNumber) {}
+
+    @PostMapping("/presign-part-url")
+    public String getPresignPartUrl(@RequestBody PresignUploadRequest request) {
+        return mediaUploadService.generatePresignedPartUrl(request.objectKey, request.uploadId, request.partNumber);
+    }
+
+    public record CompleteMultipartUploadRequest(
+            String objectKey,
+            String uploadId,
+            List<MediaUploadService.UploadedPart> uploadedParts) {}
+
+    @PostMapping("/complete")
+    public void completeUpload(@RequestBody CompleteMultipartUploadRequest request) {
+        mediaUploadService.completeMultipartUpload(request.objectKey, request.uploadId, request.uploadedParts);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
