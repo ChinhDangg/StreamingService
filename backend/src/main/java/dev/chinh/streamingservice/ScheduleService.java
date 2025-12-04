d52473f3014612c8e54d0e56ec8c862d98efab63
chinh <dangchinh645@gmail.com>
2025-12-03 20:30:02 -0500

    add clean up to expired uploading session (clean uploaded files and caches

--- backend/src/main/java/dev/chinh/streamingservice/ScheduleService.java
+++ backend/src/main/java/dev/chinh/streamingservice/ScheduleService.java
@@ -4,11 +4,14 @@ import dev.chinh.streamingservice.content.constant.MediaJobStatus;
 import dev.chinh.streamingservice.content.service.AlbumService;
 import dev.chinh.streamingservice.content.service.VideoService;
 import dev.chinh.streamingservice.data.service.ThumbnailService;
+import dev.chinh.streamingservice.upload.MediaUploadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
@@ -20,13 +23,14 @@ public class ScheduleService {
     private final VideoService videoService;
     private final AlbumService albumService;
     private final ThumbnailService thumbnailService;
+    private final MediaUploadService mediaUploadService;
 
     @Scheduled(fixedRate = 60_000, initialDelay = 60_000)
     public void scheduled() throws Exception {
         stopNonViewingVideoRunningJob();
         cleanThumbnails();
         removeAlbumCreationInfo();
-        Thread.sleep(1_000);
+        removeExpiredUploadSession();
         OSUtil.refreshUsableMemory();
     }
 
@@ -86,6 +90,31 @@ public class ScheduleService {
 
             albumService.removeAlbumCacheLastAccess(albumJobId);
             albumService.removeCacheAlbumCreationInfo(albumJobId);
+        }
+    }
+
+    private void removeExpiredUploadSession() throws Exception {
+        long now = System.currentTimeMillis();
+        Set<ZSetOperations.TypedTuple<Object>> lastAccessSessions = mediaUploadService.getAllUploadSessionCacheLastAccess(now);
+        for (ZSetOperations.TypedTuple<Object> session : lastAccessSessions) {
+            if (now - session.getScore() < 60_000) {
+                break;
+            }
+
+            String sessionId = Objects.requireNonNull(session.getValue()).toString();
+            Map<Object, Object> objectMap = mediaUploadService.getUploadSessionObjects(sessionId);
+            if (objectMap.isEmpty()) continue;
+
+            mediaUploadService.removeCacheMediaSessionRequest(sessionId);
+            mediaUploadService.removeUploadSessionCacheLastAccess(sessionId);
+
+            Set<String> seen = new HashSet<>();
+            for (Object value : objectMap.values()) {
+                String objectName = value.toString();
+                if (seen.add(objectName)) {
+                    mediaUploadService.removeUploadObject(objectName);
+                }
+            }
         }
     }
 }
