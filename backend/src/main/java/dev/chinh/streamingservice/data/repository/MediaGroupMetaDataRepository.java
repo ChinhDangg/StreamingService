50c726180243f01dc47b9278c3ccf77873d72ede
chinh <dangchinh645@gmail.com>
2025-12-08 15:30:26 -0500

    add methods to increment and retrieve num info

--- backend/src/main/java/dev/chinh/streamingservice/data/repository/MediaGroupMetaDataRepository.java
+++ backend/src/main/java/dev/chinh/streamingservice/data/repository/MediaGroupMetaDataRepository.java
@@ -5,6 +5,7 @@ import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -17,4 +18,11 @@ public interface MediaGroupMetaDataRepository extends JpaRepository<MediaGroupMe
         WHERE m.grouperMetaDataId = :grouperId
     """)
     Slice<Long> findMediaMetadataIdsByGrouperMetaDataId(@Param("grouperId") Long grouperId, Pageable pageable);
+
+    @Modifying
+    @Query("UPDATE MediaGroupMetaData m SET m.numInfo = m.numInfo + 1 WHERE m.mediaMetaDataId = :mediaId")
+    void incrementNumInfo(@Param("mediaId") Long mediaId);
+
+    @Query("SELECT m.numInfo FROM MediaGroupMetaData m WHERE m.mediaMetaDataId = :mediaId")
+    int getNumInfo(Long mediaId);
 }
