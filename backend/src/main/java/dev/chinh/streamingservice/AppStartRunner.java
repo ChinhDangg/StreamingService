fa386dd199692e48f4941b69834ea03e54582f04
chinh <dangchinh645@gmail.com>
2025-12-08 21:31:11 -0500

    add to initialize opensearch needed indexes when the application starts

--- backend/src/main/java/dev/chinh/streamingservice/AppStartRunner.java
+++ backend/src/main/java/dev/chinh/streamingservice/AppStartRunner.java
@@ -1,14 +1,20 @@
 package dev.chinh.streamingservice;
 
+import dev.chinh.streamingservice.search.service.OpenSearchService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class AppStartRunner implements ApplicationRunner {
+
+    private final OpenSearchService openSearchService;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        OSUtil.initializeRAMInfo();
+        OSUtil._initializeRAMInfo();
+        openSearchService._initializeIndexes();
     }
 }
