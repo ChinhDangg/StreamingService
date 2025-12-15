aed1d23f3b4e3f849135e5e91e1b85261efee46d
chinh <dangchinh645@gmail.com>
2025-12-15 00:00:50 -0500

    remove redundant and unused endpoints

--- backend/src/main/java/dev/chinh/streamingservice/content/controller/AlbumController.java
+++ backend/src/main/java/dev/chinh/streamingservice/content/controller/AlbumController.java
@@ -1,6 +1,6 @@
 package dev.chinh.streamingservice.content.controller;
 
-import dev.chinh.streamingservice.content.constant.Resolution;
+import dev.chinh.streamingservice.common.constant.Resolution;
 import dev.chinh.streamingservice.content.service.AlbumService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -18,11 +18,11 @@ public class AlbumController {
     public ResponseEntity<?> getAlbum(@PathVariable Long id,
                                       @PathVariable Resolution resolution,
                                       HttpServletRequest request) throws Exception {
-        return ResponseEntity.ok().body(albumService.getAllMediaInAnAlbum(id, resolution, request));
+        return ResponseEntity.ok().body(albumService.getAllMediaUrlInAnAlbum(id, resolution, request));
     }
 
-    @PostMapping("/{id}/{resolution}/{offset}/check-resized")
-    public ResponseEntity<Integer> checkResizedImage(@PathVariable Long id,
+    @GetMapping("/{id}/{resolution}/{offset}/check-resized")
+    public ResponseEntity<?> checkResizedImage(@PathVariable Long id,
                                                   @PathVariable Resolution resolution,
                                                   @PathVariable Integer offset,
                                                   HttpServletRequest request) throws Exception {
@@ -36,19 +36,5 @@ public class AlbumController {
                                                    @PathVariable Resolution vidRes,
                                                    HttpServletRequest request) throws Exception {
         return ResponseEntity.ok().body(albumService.getAlbumPartialVideoUrl(albumId, albumRes, vidNum, vidRes, request));
-    }
-
-    @GetMapping("/original/{id}")
-    public ResponseEntity<Void> getOriginalUrl(@PathVariable Long id,
-                                               @RequestParam(name = "key") String imagePath) throws Exception {
-        return albumService.getOriginalImageURLAsRedirectResponse(id, imagePath, 30 * 60);
-    }
-
-    @GetMapping("/resize/{id}")
-    public ResponseEntity<Void> getResizedImage(@PathVariable Long id,
-                                                @RequestParam Resolution res,
-                                                @RequestParam(name = "key") String imagePath,
-                                                HttpServletRequest request) throws Exception {
-        return albumService.processResizedImageURLAsRedirectResponse(id, imagePath, res, request);
     }
 }
