3c28039d8fec7b07822bea2771e387073c83acdc
chinh <dangchinh645@gmail.com>
2025-12-16 21:55:09 -0500

    use executorservice for async job instead of new thread

---
+++ workers/src/main/java/dev/chinh/streamingservice/config/MediaProcessingConfig.java
@@ -0,0 +1,17 @@
+package dev.chinh.streamingservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Configuration
+public class MediaProcessingConfig {
+
+    @Bean(name = "ffmpegExecutor", destroyMethod = "shutdown")
+    public ExecutorService ffmpegExecutor() {
+        int cores = Runtime.getRuntime().availableProcessors();
+        return Executors.newFixedThreadPool(Math.max(1, cores / 4));
+    }
+}
