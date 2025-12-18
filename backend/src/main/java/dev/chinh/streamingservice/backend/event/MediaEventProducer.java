c955df874eefda13ecf411f2ca37bdd5936c8941
chinh <dangchinh645@gmail.com>
2025-12-17 23:23:15 -0500

    add event publishing to add, update, delete name entity from opensearch

--- backend/src/main/java/dev/chinh/streamingservice/backend/event/MediaEventProducer.java
+++ backend/src/main/java/dev/chinh/streamingservice/backend/event/MediaEventProducer.java
@@ -1,6 +1,7 @@
 package dev.chinh.streamingservice.backend.event;
 
 import dev.chinh.streamingservice.backend.event.config.KafkaRedPandaConfig;
+import dev.chinh.streamingservice.common.event.MediaUpdateEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -13,25 +14,40 @@ public class MediaEventProducer {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    private static final String mediaUpdateOpenSearchTopic = KafkaRedPandaConfig.mediaUpdatedOpenSearchTopic;
+    private static final String MEDIA_UPDATED_OPENSEARCH_TOPIC = KafkaRedPandaConfig.MEDIA_UPDATED_OPENSEARCH_TOPIC;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void publishUpdateLengthOpenSearch(MediaUpdateEvent.LengthUpdated event) {
-        kafkaTemplate.send(mediaUpdateOpenSearchTopic, event);
+        kafkaTemplate.send(MEDIA_UPDATED_OPENSEARCH_TOPIC, event);
     }
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void publishCreateMediaIndexOpenSearch(MediaUpdateEvent.MediaCreated event) {
-        kafkaTemplate.send(mediaUpdateOpenSearchTopic, event);
+        kafkaTemplate.send(MEDIA_UPDATED_OPENSEARCH_TOPIC, event);
     }
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void publishUpdateMediaNameEntityOpenSearch(MediaUpdateEvent.MediaNameEntityUpdated event) {
-        kafkaTemplate.send(mediaUpdateOpenSearchTopic, event);
+        kafkaTemplate.send(MEDIA_UPDATED_OPENSEARCH_TOPIC, event);
     }
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void publishUpdateMediaTitleOpenSearch(MediaUpdateEvent.MediaTitleUpdated event) {
-        kafkaTemplate.send(mediaUpdateOpenSearchTopic, event);
+        kafkaTemplate.send(MEDIA_UPDATED_OPENSEARCH_TOPIC, event);
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void publishCreateNameEntityIndexOpenSearch(MediaUpdateEvent.NameEntityCreated event) {
+        kafkaTemplate.send(MEDIA_UPDATED_OPENSEARCH_TOPIC, event);
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void publishUpdateNameEntityIndexOpenSearch(MediaUpdateEvent.NameEntityUpdated event) {
+        kafkaTemplate.send(MEDIA_UPDATED_OPENSEARCH_TOPIC, event);
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void publishDeleteNameEntityIndexOpenSearch(MediaUpdateEvent.NameEntityDeleted event) {
+        kafkaTemplate.send(MEDIA_UPDATED_OPENSEARCH_TOPIC, event);
     }
 }
