0910a267a5eae273df8a892671500dd2d86811b8
chinh <dangchinh645@gmail.com>
2025-12-17 23:21:33 -0500

    remove object mapper

--- search-client/src/main/java/dev/chinh/streamingservice/searchclient/config/OpenSearchConfig.java
+++ search-client/src/main/java/dev/chinh/streamingservice/searchclient/config/OpenSearchConfig.java
@@ -1,10 +1,5 @@
 package dev.chinh.streamingservice.searchclient.config;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.hc.client5.http.auth.AuthScope;
 import org.apache.hc.client5.http.auth.UsernamePasswordCredentials;
 import org.apache.hc.client5.http.impl.auth.BasicCredentialsProvider;
@@ -39,16 +34,4 @@ public class OpenSearchConfig {
 
         return new OpenSearchClient(transport);
     }
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
-        mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
-        //mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false); to ignore not mapping fields
-        return mapper;
-    }
-
 }
