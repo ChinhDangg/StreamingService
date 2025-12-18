edd4f186e90f764663fae060e53e8f0229369755
chinh <dangchinh645@gmail.com>
2025-12-17 23:05:20 -0500

    move media update event to common package

---
+++ common/src/main/java/dev/chinh/streamingservice/common/event/MediaUpdateEvent.java
@@ -0,0 +1,42 @@
+package dev.chinh.streamingservice.common.event;
+
+
+import dev.chinh.streamingservice.common.constant.MediaNameEntityConstant;
+
+public interface MediaUpdateEvent {
+
+    record LengthUpdated(
+            long mediaId,
+            Integer newLength
+    ) implements MediaUpdateEvent {}
+
+    record MediaCreated(
+            long mediaId,
+            boolean isGrouper
+    ) implements MediaUpdateEvent{}
+
+    record MediaNameEntityUpdated(
+            long mediaId,
+            MediaNameEntityConstant nameEntityConstant
+    ) implements MediaUpdateEvent{}
+
+    record MediaTitleUpdated(
+            long mediaId
+    ) implements MediaUpdateEvent{}
+
+
+    record NameEntityCreated(
+            MediaNameEntityConstant nameEntityConstant,
+            long nameEntityId
+    ) implements MediaUpdateEvent{}
+
+    record NameEntityUpdated(
+            MediaNameEntityConstant nameEntityConstant,
+            long nameEntityId
+    ) implements MediaUpdateEvent{}
+
+    record NameEntityDeleted(
+            MediaNameEntityConstant nameEntityConstant,
+            long nameEntityId
+    ) implements MediaUpdateEvent{}
+}
