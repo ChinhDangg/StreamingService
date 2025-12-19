9e2878315767814a232083dc8be40e1308392879
chinh <dangchinh645@gmail.com>
2025-12-19 16:27:45 -0500

    remove datasource config in class level

--- media-upload/src/main/java/dev/chinh/streamingservice/mediaupload/config/PersistenceJpaConfig.java
+++ media-upload/src/main/java/dev/chinh/streamingservice/mediaupload/config/PersistenceJpaConfig.java
@@ -1,13 +1,8 @@
 package dev.chinh.streamingservice.mediaupload.config;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableJpaRepositories(
@@ -17,19 +12,4 @@ import javax.sql.DataSource;
         basePackages = "dev.chinh.streamingservice.persistence.entity"
 )
 public class PersistenceJpaConfig {
-
-    @Bean
-    public DataSource dataSource() {
-        HikariConfig config = new HikariConfig();
-
-        config.setJdbcUrl("jdbc:postgresql://localhost:5432/media_db");
-        config.setUsername("myuser");
-        config.setPassword("secret");
-        config.setDriverClassName("org.postgresql.Driver");
-
-        config.setMaximumPoolSize(10);
-        config.setMinimumIdle(2);
-        config.setPoolName("media-db-pool");
-        return new HikariDataSource(config);
-    }
 }
