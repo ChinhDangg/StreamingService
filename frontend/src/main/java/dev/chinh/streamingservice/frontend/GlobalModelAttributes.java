85f2fe8143b9c9b269992fd8a397664abc94e1c7
chinh <dangchinh645@gmail.com>
2025-12-20 19:58:26 -0500

    add modifying allow option as global attribute depend on the role

--- frontend/src/main/java/dev/chinh/streamingservice/frontend/GlobalModelAttributes.java
+++ frontend/src/main/java/dev/chinh/streamingservice/frontend/GlobalModelAttributes.java
@@ -1,5 +1,6 @@
 package dev.chinh.streamingservice.frontend;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
@@ -24,7 +25,7 @@ public class GlobalModelAttributes {
     }
 
     @ModelAttribute("modifyAllow")
-    public boolean modifyAllow(Principal principal) {
-        return true;
+    public boolean modifyAllow(Authentication authentication) {
+        return authentication != null && authentication.getAuthorities().stream().anyMatch(o -> o.getAuthority().equals("ADMIN"));
     }
 }
