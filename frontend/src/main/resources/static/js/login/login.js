f3bf45f3ab805b5dd6b0a37aac4bbc4af4d4ec7b
chinh <dangchinh645@gmail.com>
2025-12-20 19:23:27 -0500

    add login page js

---
+++ frontend/src/main/resources/static/js/login/login.js
@@ -0,0 +1,21 @@
+document.getElementById('login-form').addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const login = {
+        username: document.getElementById('email').value,
+        password: document.getElementById('password').value,
+    }
+    const response = await fetch('/auth/login', {
+        method: 'POST',
+        credentials: "include",
+        body: JSON.stringify(login),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    if (response.ok) {
+        const params = new URLSearchParams(window.location.search);
+        window.location.href = params.get('r') || '/';
+    } else if (response.status === 401) {
+        document.getElementById('login-error').classList.remove('hidden');
+    }
+});
