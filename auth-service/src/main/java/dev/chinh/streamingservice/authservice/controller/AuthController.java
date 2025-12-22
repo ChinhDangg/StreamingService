982d14c9209087e8a18d89254526d95746ac02b2
chinh <dangchinh645@gmail.com>
2025-12-22 00:28:34 -0500

    add csrf token generation when login. add refresh token for api

--- auth-service/src/main/java/dev/chinh/streamingservice/authservice/controller/AuthController.java
+++ auth-service/src/main/java/dev/chinh/streamingservice/authservice/controller/AuthController.java
@@ -2,6 +2,8 @@ package dev.chinh.streamingservice.authservice.controller;
 
 import dev.chinh.streamingservice.authservice.service.AuthenticationRequest;
 import dev.chinh.streamingservice.authservice.service.TokenService;
+import dev.chinh.streamingservice.authservice.user.SecurityUser;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -10,6 +12,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,19 +28,32 @@ public class AuthController {
     private final TokenService tokenService;
 
     @PostMapping("/login")
-    public void authenticate(@RequestBody AuthenticationRequest request, HttpServletResponse response) {
-        String isValid = request.isValid();
+    public void authenticate(@RequestBody AuthenticationRequest authRequest, HttpServletRequest request, HttpServletResponse response) {
+        String isValid = authRequest.isValid();
         if (isValid != null) {
             throw new BadCredentialsException(isValid);
         }
-        UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(request.username(), request.password());
-        Authentication authentication = authenticationManager.authenticate(authRequest);
-        ResponseCookie responseCookie = tokenService.makeAuthenticateCookie(authentication);
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(authRequest.username(), authRequest.password());
+        Authentication authentication = authenticationManager.authenticate(authToken);
 
-        System.out.println(responseCookie.getName());
-        System.out.println(responseCookie.getValue());
+        CsrfToken csrfToken = addCsrfCookie(request, response);
 
-        response.addHeader(HttpHeaders.SET_COOKIE, responseCookie.toString());
+        System.out.println(csrfToken.getToken());
+
+        tokenService.issueTokenCookies((SecurityUser) authentication.getPrincipal(), response);
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    @PostMapping("/refresh")
+    public void refreshAccessToken(HttpServletRequest request, HttpServletResponse response) {
+        ResponseCookie refreshCookie = tokenService.getRefreshAccessToken(request);
+        if (refreshCookie == null) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+        System.out.println("Got refresh cookie");
+        addCsrfCookie(request, response);
+        response.addHeader(HttpHeaders.SET_COOKIE, refreshCookie.toString());
         response.setStatus(HttpServletResponse.SC_OK);
     }
 
@@ -44,5 +61,17 @@ public class AuthController {
     public void logout(HttpServletResponse response) {
         response.addHeader(HttpHeaders.SET_COOKIE, TokenService.removeAuthCookie().toString());
         response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+    }
+
+    private CsrfToken addCsrfCookie(HttpServletRequest request, HttpServletResponse response) {
+        // add csrf token
+        CookieCsrfTokenRepository csrfRepo = CookieCsrfTokenRepository.withHttpOnlyFalse();
+        csrfRepo.setCookieCustomizer(cookie -> cookie
+                .sameSite("Strict")
+                .path("/")
+        );
+        CsrfToken csrfToken = csrfRepo.generateToken(request);
+        csrfRepo.saveToken(csrfToken, request, response);
+        return csrfToken;
     }
 }
--- auth-service/src/main/java/dev/chinh/streamingservice/authservice/service/TokenService.java
+++ auth-service/src/main/java/dev/chinh/streamingservice/authservice/service/TokenService.java
@@ -1,16 +1,19 @@
 package dev.chinh.streamingservice.authservice.service;
 
+import dev.chinh.streamingservice.authservice.user.SecurityUser;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -18,36 +21,36 @@ import java.util.List;
 public class TokenService {
 
     private final JwtEncoder jwtEncoder;
+    private final JwtDecoder jwtDecoder;
+    private final JpaUserDetailService userDetailsService;
+    private final RedisTemplate<String, String> redisStringTemplate;
 
-    private String generateToken(Authentication authentication) {
+    private String generateToken(SecurityUser securityUser, long tokenExpirySeconds) {
         Instant now = Instant.now();
-        long expirySeconds = 900; // 15 mins
 
-        String username = authentication.getName();
-
-        List<String> roles = authentication.getAuthorities().stream()
+        List<String> roles = securityUser.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .toList();
 
         JwtClaimsSet claimsSet = JwtClaimsSet.builder()
                 .issuer("ss-media-auth") // auth-service name
                 .issuedAt(now)
-                .expiresAt(now.plusSeconds(expirySeconds))
-                .subject(username)
+                .expiresAt(now.plusSeconds(tokenExpirySeconds))
+                .subject(securityUser.getUserId().toString())
                 .claim("roles", roles)
                 .build();
 
         return this.jwtEncoder.encode(JwtEncoderParameters.from(claimsSet)).getTokenValue();
     }
 
-    private static final String AUTH_COOKIE_NAME = "Auth";
+    private static final String ACCESS_AUTH_COOKIE_NAME = "Auth";
+    private static final String REFRESH_AUTH_COOKIE_NAME = "Refresh";
 
-    public ResponseCookie makeAuthenticateCookie(Authentication authentication) {
-        long expirySeconds = 60 * 60; // 1 hour
+    private ResponseCookie makeAuthenticateCookie(SecurityUser securityUser, String cookieName, long expirySeconds) {
 
-        String token = generateToken(authentication);
-        return ResponseCookie.from(AUTH_COOKIE_NAME, token)
-                .maxAge(expirySeconds)
+        String token = generateToken(securityUser, expirySeconds);
+        return ResponseCookie.from(cookieName, token)
+                .maxAge(expirySeconds + 60)
                 .httpOnly(true)
                 .sameSite("Strict")
                 .secure(false) // change to true in production
@@ -55,8 +58,68 @@ public class TokenService {
                 .build();
     }
 
+    private ResponseCookie makeRefreshTokenCookie(SecurityUser securityUser) {
+        return makeAuthenticateCookie(securityUser, REFRESH_AUTH_COOKIE_NAME, 24 * 60 * 60); // 24 hours
+    }
+
+    private ResponseCookie makeAccessTokenCookie(SecurityUser securityUser) {
+        return makeAuthenticateCookie(securityUser, ACCESS_AUTH_COOKIE_NAME, 60 * 15); // 15 minutes
+    }
+
+    public void issueTokenCookies(SecurityUser securityUser, HttpServletResponse response) {
+        ResponseCookie refreshCookie = makeRefreshTokenCookie(securityUser);
+        ResponseCookie accessCookie = makeAccessTokenCookie(securityUser);
+        System.out.println(refreshCookie);
+        System.out.println(accessCookie);
+
+        redisStringTemplate.opsForValue().set("refresh:" + securityUser.getUserId(), refreshCookie.getValue());
+
+        response.addHeader(HttpHeaders.SET_COOKIE, refreshCookie.toString());
+        response.addHeader(HttpHeaders.SET_COOKIE, accessCookie.toString());
+    }
+
+    public ResponseCookie getRefreshAccessToken(HttpServletRequest request) {
+        Cookie refreshCookie = Arrays.stream(request.getCookies())
+                .filter(c -> REFRESH_AUTH_COOKIE_NAME.equals(c.getName()))
+                .findFirst()
+                .orElse(null);
+        String refreshTokenCookie = refreshCookie != null ? refreshCookie.getValue() : null;
+
+        if (refreshTokenCookie == null) {
+            System.out.println("No refresh token cookie in request");
+            return null;
+        }
+
+        Jwt jwt = jwtDecoder.decode(refreshTokenCookie);
+        long userId = Long.parseLong(jwt.getSubject());
+
+        SecurityUser user = userDetailsService.findById(userId).orElse(null);
+        if (user == null) {
+            System.out.println("User not found with id: " + userId);
+            return null;
+        }
+
+        String storedRefreshToken = redisStringTemplate.opsForValue().get("refresh:" + userId);
+
+        if (storedRefreshToken != null && storedRefreshToken.equals(refreshTokenCookie)) {
+            String csrfTokenHeader = request.getHeader("X-XSRF-TOKEN");
+            Cookie csrfCookie = Arrays.stream(request.getCookies())
+                    .filter(c -> "XSRF-TOKEN".equals(c.getName()))
+                    .findFirst()
+                    .orElse(null);
+            String csrfTokenCookie = csrfCookie != null ? csrfCookie.getValue() : null;
+            if (csrfTokenHeader != null && csrfTokenHeader.equals(csrfTokenCookie)) {
+                return makeAccessTokenCookie(user);
+            }
+            System.out.println("CSRF token mismatch: " + csrfTokenHeader + " " + csrfTokenCookie);
+            return null;
+        }
+        System.out.println("Refresh token mismatch: " + storedRefreshToken + " " + refreshTokenCookie);
+        return null;
+    }
+
     public static Cookie removeAuthCookie() {
-        Cookie cookie = new Cookie(AUTH_COOKIE_NAME, null);
+        Cookie cookie = new Cookie(ACCESS_AUTH_COOKIE_NAME, null);
         cookie.setMaxAge(0);
         cookie.setPath("/");
         return cookie;
