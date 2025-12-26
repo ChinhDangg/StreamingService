02cabf11fcebea200cac83e4a07f0151a4d230d9
chinh <dangchinh645@gmail.com>
2025-12-26 00:43:31 -0500

    add default user with test credentials

--- auth-service/src/main/java/dev/chinh/streamingservice/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/dev/chinh/streamingservice/authservice/AuthServiceApplication.java
@@ -6,6 +6,7 @@ import dev.chinh.streamingservice.authservice.repository.UserRepository;
 import jakarta.annotation.PostConstruct;
 import org.bouncycastle.util.io.pem.PemObject;
 import org.bouncycastle.util.io.pem.PemWriter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -26,23 +27,27 @@ public class AuthServiceApplication {
         SpringApplication.run(AuthServiceApplication.class, args);
     }
 
-//    @Bean
-//    CommandLineRunner commandLineRunner(PasswordEncoder passwordEncoder,
-//                                        UserRepository userRepository,
-//                                        RSAPublicKey publicKey, JwtEncoder encoder, JwtDecoder decoder) {
-//        return args -> {
-////            User user = new User();
-////            user.setEmail("test@email.com");
-////            String password = "SomePass123!";
-////            user.setPassword(passwordEncoder.encode(password));
-////            user.setUsername("Test User");
-////            user.setRole(Role.ADMIN);
-////            userRepository.save(user);
-//
-////            printKeys(publicKey, encoder, decoder);
-//
-//        };
-//    }
+    @Bean
+    CommandLineRunner commandLineRunner(PasswordEncoder passwordEncoder,
+                                        UserRepository userRepository,
+                                        @Value("${test-user.username}") String testUsername,
+                                        @Value("${test-user.password}") String testPassword,
+                                        @Value("${test-user.roles}") String testRole,
+                                        @Value("${test-user.name}") String testName,
+                                        RSAPublicKey publicKey, JwtEncoder encoder, JwtDecoder decoder) {
+        return args -> {
+            try {
+                User user = new User();
+                user.setEmail(testUsername);
+                user.setPassword(passwordEncoder.encode(testPassword));
+                user.setUsername(testName);
+                user.setRole(Role.valueOf(testRole));
+                userRepository.save(user);
+            } catch (Exception ignored) {
+            }
+//            printKeys(publicKey, encoder, decoder);
+        };
+    }
 
     public void printKeys(RSAPublicKey rsaPublicKey, JwtEncoder encoder, JwtDecoder decoder) throws Exception {
         System.out.println("=== PUBLIC KEY (PEM) ===");
