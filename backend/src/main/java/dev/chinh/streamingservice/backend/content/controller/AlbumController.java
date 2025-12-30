cc7c324bee28ce4521ec96a8cf31785da7b72503
chinh <dangchinh645@gmail.com>
2025-12-29 22:04:23 -0500

    add option to always show original resolution for media

--- backend/src/main/java/dev/chinh/streamingservice/backend/content/controller/AlbumController.java
+++ backend/src/main/java/dev/chinh/streamingservice/backend/content/controller/AlbumController.java
@@ -4,6 +4,7 @@ import dev.chinh.streamingservice.common.constant.Resolution;
 import dev.chinh.streamingservice.backend.content.service.AlbumService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,10 +15,17 @@ public class AlbumController {
 
     private final AlbumService albumService;
 
+    @Value("${always-show-original-resolution}")
+    private String alwaysShowOriginalResolution;
+
     @GetMapping("/{id}/{resolution}")
     public ResponseEntity<?> getAlbum(@PathVariable Long id,
                                       @PathVariable Resolution resolution,
                                       HttpServletRequest request) throws Exception {
+        if (Boolean.parseBoolean(alwaysShowOriginalResolution)) {
+            // if always show original - intercept the resolution parameter
+            resolution = Resolution.original;
+        }
         return ResponseEntity.ok().body(albumService.getAllMediaUrlInAnAlbum(id, resolution, request));
     }
 
@@ -26,6 +34,9 @@ public class AlbumController {
                                                   @PathVariable Resolution resolution,
                                                   @PathVariable Integer offset,
                                                   HttpServletRequest request) throws Exception {
+        if (Boolean.parseBoolean(alwaysShowOriginalResolution)) {
+            return ResponseEntity.ok().body("99999");
+        }
         return ResponseEntity.ok().body(albumService.processResizedAlbumImagesInBatch(id, resolution, offset, 5, request));
     }
 
--- backend/src/main/java/dev/chinh/streamingservice/backend/content/controller/VideoController.java
+++ backend/src/main/java/dev/chinh/streamingservice/backend/content/controller/VideoController.java
@@ -3,6 +3,7 @@ package dev.chinh.streamingservice.backend.content.controller;
 import dev.chinh.streamingservice.common.constant.Resolution;
 import dev.chinh.streamingservice.backend.content.service.VideoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,10 +14,12 @@ public class VideoController {
 
     private final VideoService videoService;
 
+    @Value("${always-show-original-resolution}")
+    private String alwaysShowOriginalResolution;
+
     @GetMapping("/original/{id}")
     public ResponseEntity<String> getVideoUrl(@PathVariable Long id) throws Exception {
-        String url = videoService.getOriginalVideoUrl(id);
-        return ResponseEntity.ok(url);
+        return ResponseEntity.ok(videoService.getOriginalVideoUrl(id));
     }
 
     @GetMapping("/preview/{id}")
@@ -27,6 +30,9 @@ public class VideoController {
     @GetMapping("/partial/{id}/{resolution}")
     public ResponseEntity<String> getVideoAtDifferentResolutionUrl(@PathVariable Long id,
                                                     @PathVariable Resolution resolution) throws Exception {
+        if (Boolean.parseBoolean(alwaysShowOriginalResolution)) {
+            return ResponseEntity.ok(videoService.getOriginalVideoUrl(id));
+        }
         return ResponseEntity.ok(videoService.getPartialVideoUrl(id, resolution));
     }
 
