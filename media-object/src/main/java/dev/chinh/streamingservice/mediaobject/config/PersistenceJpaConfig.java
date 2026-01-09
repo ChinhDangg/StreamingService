01713dbf6b770c1348414ff757a22831043f19a7
chinh <dangchinh645@gmail.com>
2026-01-08 19:28:35 -0500

    add persistence jpa config and dependencies

---
+++ media-object/src/main/java/dev/chinh/streamingservice/mediaobject/config/PersistenceJpaConfig.java
@@ -0,0 +1,15 @@
+package dev.chinh.streamingservice.mediaobject.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(
+        basePackages = "dev.chinh.streamingservice.persistence.repository"
+)
+@EntityScan(
+        basePackages = "dev.chinh.streamingservice.persistence.entity"
+)
+public class PersistenceJpaConfig {
+}
