5107667a41251073d1ac45ee9f876d45ff9f5667
chinh <dangchinh645@gmail.com>
2026-01-13 00:42:48 -0500

    bug fix ffmpeg generating thumbnail from video - allow YUV range for MJPEG as non-standard. also handle short video duration rather than arbitrary set desire thumbnail seek at 2

--- media-object/src/main/java/dev/chinh/streamingservice/mediaobject/ThumbnailService.java
+++ media-object/src/main/java/dev/chinh/streamingservice/mediaobject/ThumbnailService.java
@@ -25,23 +25,26 @@ public class ThumbnailService {
     private static final String defaultVidPath = "vid";
     private static final String defaultAlbumPath = "album";
 
-    public String generateThumbnailFromVideo(String bucket, String objectName) throws Exception {
+    public String generateThumbnailFromVideo(String bucket, String objectName, int videoLength) throws Exception {
 
         String thumbnailObject = objectName.substring(0, objectName.lastIndexOf(".")) + "-thumb.jpg";
         String thumbnailOutput = OSUtil.normalizePath(diskDir, thumbnailObject);
         Files.createDirectories(Path.of(thumbnailOutput.substring(0, thumbnailOutput.lastIndexOf("/"))));
         String videoInput = minIOService.getObjectUrlForContainer(bucket, objectName);
 
+        videoLength = videoLength >= 2 ? videoLength : 0;
+
         List<String> command = Arrays.asList(
                 "docker", "exec", "ffmpeg",
                 "ffmpeg",
                 "-v", "error",                 // only show errors, no logs
+                "-ss", String.valueOf(videoLength), // FAST SEEK: jump to keyframe nearest the 1-second mark
                 "-skip_frame", "nokey",        // skip NON-keyframes → decode only I-frames - fast operation
-                "-ss", "2",                    // FAST SEEK: jump to keyframe nearest the 1-second mark
                 "-i", videoInput,              // input video
                 "-frames:v", "1",              // output exactly one frame
                 "-vsync", "vfr",               // variable frame rate; ensures correct frame extraction
                 "-q:v", "2",                   // high JPEG quality (2 = very high, 1 = max)
+                "-strict", "unofficial",       // Allows non-standard YUV range for MJPEG
                 // select only keyframes (only I-frames). Escaped for Java.
                 "-vf", "select='eq(pict_type\\,I)'",
                 thumbnailOutput                // output thumbnail (original resolution)
