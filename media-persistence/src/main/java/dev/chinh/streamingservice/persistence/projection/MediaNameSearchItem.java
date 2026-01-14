9dc618d249062cf452c2c64574c8c2cad7c0beac
chinh <dangchinh645@gmail.com>
2026-01-14 00:43:40 -0500

    add no arg constructor since object mapper use field only for mapping - not constructor

--- media-persistence/src/main/java/dev/chinh/streamingservice/persistence/projection/MediaNameSearchItem.java
+++ media-persistence/src/main/java/dev/chinh/streamingservice/persistence/projection/MediaNameSearchItem.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import dev.chinh.streamingservice.common.data.ContentMetaData;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class MediaNameSearchItem {
 
