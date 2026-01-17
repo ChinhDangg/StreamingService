2a6c0fcd6a3982ea28be134c44b96f2686bb103a
chinh <dangchinh645@gmail.com>
2026-01-16 21:40:45 -0500

    add endpoint and service to update media thumbnail

---
+++ media-upload/src/main/java/dev/chinh/streamingservice/mediaupload/modify/dto/MediaUpdateThumbnailRequest.java
@@ -0,0 +1,20 @@
+package dev.chinh.streamingservice.mediaupload.modify.dto;
+
+import dev.chinh.streamingservice.mediaupload.validation.ValidImage;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MediaUpdateThumbnailRequest {
+
+    private Integer num;
+
+    @ValidImage
+    private MultipartFile thumbnail;
+}
