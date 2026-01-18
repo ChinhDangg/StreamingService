94c1c2a8ccf441de2d48ca222e5e0fbab58f9fe8
chinh <dangchinh645@gmail.com>
2026-01-17 19:44:28 -0500

    centralize media event topics

---
+++ common/src/main/java/dev/chinh/streamingservice/common/event/EventTopics.java
@@ -0,0 +1,13 @@
+package dev.chinh.streamingservice.common.event;
+
+public class EventTopics {
+
+    public static final String MEDIA_CREATED_TOPIC = "media-created-event";
+    public static final String MEDIA_DELETED_TOPIC = "media-deleted-event";
+
+    public static final String MEDIA_THUMBNAIL_UPDATED_TOPIC = "media-thumbnail-updated-event";
+    public static final String THUMBNAIL_DELETED_TOPIC = "media-thumbnail-deleted-event";
+
+    public static final String MEDIA_SEARCH_TOPIC = "media-search-event";
+    public static final String MEDIA_OBJECT_TOPIC = "media-object-event";
+}
