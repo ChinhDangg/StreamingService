430803d220e3014fde072529751b85f01e7d787f
chinh <dangchinh645@gmail.com>
2026-02-12 22:42:55 -0500

    add method to update mid only

--- file-manager/src/main/java/dev/chinh/streamingservice/filemanager/FileSystemRepository.java
+++ file-manager/src/main/java/dev/chinh/streamingservice/filemanager/FileSystemRepository.java
@@ -11,4 +11,6 @@ public interface FileSystemRepository extends MongoRepository<FileSystemItem, St
     List<FileSystemItem> findByPath(String path);
 
     List<FileSystemItem> findByPathStartingWith(String path);
+
+    void updateMId(String id, Long mId);
 }
