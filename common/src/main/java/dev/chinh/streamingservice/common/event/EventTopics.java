cc79335be0ce2eb36b5affa060219601d5505f23
chinh <dangchinh645@gmail.com>
2026-02-12 22:41:41 -0500

    add upload service topic

--- common/src/main/java/dev/chinh/streamingservice/common/event/EventTopics.java
+++ common/src/main/java/dev/chinh/streamingservice/common/event/EventTopics.java
@@ -2,10 +2,11 @@ package dev.chinh.streamingservice.common.event;
 
 public class EventTopics {
 
-    public static final String MEDIA_OBJECT_TOPIC = "media-object-event";
     public static final String MEDIA_SEARCH_TOPIC = "media-search-event";
     public static final String MEDIA_SEARCH_AND_BACKUP_TOPIC = "media-search-and-backup-event";
     public static final String MEDIA_FILE_TOPIC = "media-file-event";
-    public static final String MEDIA_ALL_EXCEPT_OBJECT_TOPIC = "media-all-except-object-event";
+    public static final String MEDIA_SEARCH_BACKUP_AND_FILE_TOPIC = "media-search-backup-and-file-event";
+    public static final String MEDIA_OBJECT_TOPIC = "media-object-event";
+    public static final String MEDIA_UPLOAD_TOPIC = "media-upload-event";
     public static final String MEDIA_ALL_TOPIC = "media-all-event";
 }
