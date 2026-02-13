4f886b741b016268b77c53dcd8d753b43b838784
chinh <dangchinh645@gmail.com>
2026-02-12 22:42:34 -0500

    add detectFileTypeFromMediaType method

--- file-manager/src/main/java/dev/chinh/streamingservice/filemanager/FileType.java
+++ file-manager/src/main/java/dev/chinh/streamingservice/filemanager/FileType.java
@@ -1,5 +1,11 @@
 package dev.chinh.streamingservice.filemanager;
 
+import dev.chinh.streamingservice.common.constant.MediaType;
+
 public enum FileType {
-    DIR, FILE, VIDEO, ALBUM, ERROR
+    DIR, FILE, VIDEO, IMAGE, ALBUM, ERROR;
+
+    public static FileType detectFileTypeFromMediaType(MediaType mediaType) {
+        return mediaType == MediaType.VIDEO ? FileType.VIDEO : mediaType == MediaType.IMAGE ? FileType.IMAGE: FileType.FILE;
+    }
 }
