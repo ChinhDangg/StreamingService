5795bfdda4e7d4e1c29457e4cff6be3181753704
chinh <dangchinh645@gmail.com>
2026-02-15 23:25:01 -0500

    add global exception handler to file service

---
+++ file-manager/src/main/java/dev/chinh/streamingservice/filemanager/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package dev.chinh.streamingservice.filemanager.exception;
+
+import dev.chinh.streamingservice.common.exception.DuplicateEntryException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        e.printStackTrace();
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<String> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        e.printStackTrace();
+        return ResponseEntity.badRequest().body("Not supported method");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        e.printStackTrace();
+        return ResponseEntity.internalServerError().body("Internal Server Error");
+    }
+}
