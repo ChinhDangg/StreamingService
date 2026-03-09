e499250f60b4cdfce8c40e3f2919e72ba37507c6
chinh <dangchinh645@gmail.com>
2026-03-08 23:48:37 -0400

    add back missing modifyAllow model attribute

--- frontend/src/main/java/dev/chinh/streamingservice/frontend/GlobalModelAttributes.java
+++ frontend/src/main/java/dev/chinh/streamingservice/frontend/GlobalModelAttributes.java
@@ -1,10 +1,8 @@
 package dev.chinh.streamingservice.frontend;
 
-//import org.springframework.security.core.Authentication;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
-
-import java.security.Principal;
 
 @ControllerAdvice
 public class GlobalModelAttributes {
@@ -24,10 +22,10 @@ public class GlobalModelAttributes {
         return "main-grouper-container";
     }
 
-//    @ModelAttribute("modifyAllow")
-//    public boolean modifyAllow(Authentication authentication) {
-//        return authentication != null && authentication.getAuthorities().stream().anyMatch(o -> o.getAuthority().equals("ADMIN"));
-//    }
+    @ModelAttribute("modifyAllow")
+    public boolean modifyAllow(Authentication authentication) {
+        return authentication != null && authentication.getAuthorities().stream().anyMatch(o -> o.getAuthority().equals("ADMIN"));
+    }
 
     @ModelAttribute("modifyAllow")
     public boolean modifyAllow() {
