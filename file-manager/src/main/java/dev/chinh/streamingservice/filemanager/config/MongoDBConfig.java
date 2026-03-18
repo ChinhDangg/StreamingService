703680cdc7f94f2118e146b473273bb68ba7d550
chinh <dangchinh645@gmail.com>
2026-03-18 19:07:52 -0400

    remove class field info in mongodb since class is not using polymorphism

--- file-manager/src/main/java/dev/chinh/streamingservice/filemanager/config/MongoDBConfig.java
+++ file-manager/src/main/java/dev/chinh/streamingservice/filemanager/config/MongoDBConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.data.mongodb.core.convert.*;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 @Configuration
 public class MongoDBConfig {
@@ -11,5 +13,21 @@ public class MongoDBConfig {
     @Bean
     MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
         return new MongoTransactionManager(dbFactory);
+    }
+
+    @Bean
+    public MappingMongoConverter mappingMongoConverter(MongoDatabaseFactory factory,
+                                                       MongoMappingContext context,
+                                                       MongoCustomConversions conversions) {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(factory);
+        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, context);
+        converter.setCustomConversions(conversions);
+
+        // Create a TypeMapper with a NULL type key
+        // This tells Spring "Do not write any class information to the document"
+        // Only use if not using polymorphism
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+
+        return converter;
     }
 }
