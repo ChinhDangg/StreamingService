95dcb35d3df07c4829697872e9acae63642690d0
chinh <dangchinh645@gmail.com>
2026-04-06 19:53:02 -0400

    update mongo connection factory to use configuration in java code instead of application.properties

--- file-manager/src/main/java/dev/chinh/streamingservice/filemanager/config/MongoDBConfig.java
+++ file-manager/src/main/java/dev/chinh/streamingservice/filemanager/config/MongoDBConfig.java
@@ -1,14 +1,60 @@
 package dev.chinh.streamingservice.filemanager.config;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 import org.springframework.data.mongodb.core.convert.*;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
+import java.util.Collections;
+
 @Configuration
 public class MongoDBConfig {
+
+    @Value("${MONGODB_USERNAME}")
+    private String username;
+
+    @Value("${MONGODB_PASSWORD}")
+    private String password;
+
+    @Value("${MONGODB_AUTH_SOURCE}")
+    private String authSource;
+
+    @Value("${MONGODB_HOST}")
+    private String host;
+
+    @Value("${MONGODB_PORT}")
+    private int port;
+
+    @Value("${MONGODB_DATABASE}")
+    private String database;
+
+    @Bean
+    public MongoDatabaseFactory mongoDbFactory() {
+        // 1. Create Credentials (handles special characters automatically)
+        MongoCredential credential = MongoCredential.createCredential(
+                username,
+                authSource,
+                password.toCharArray()
+        );
+
+        // 2. Configure Client Settings
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyToClusterSettings(builder ->
+                        builder.hosts(Collections.singletonList(new ServerAddress(host, port))))
+                .credential(credential)
+                .build();
+
+        // 3. Return a Factory that points to your specific database
+        return new SimpleMongoClientDatabaseFactory(MongoClients.create(settings), database);
+    }
 
     @Bean
     public MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
