e10b2b40efa59be6f8357d57e6278804912a35c4
chinh <dangchinh645@gmail.com>
2026-07-04 20:17:48 +0700

    update search cache to avoid overriding existing cache that could be cached by the database (source of truth) and only reset the ttl instead

--- backend/src/main/java/dev/chinh/streamingservice/backend/search/service/MediaSearchCacheService.java
+++ backend/src/main/java/dev/chinh/streamingservice/backend/search/service/MediaSearchCacheService.java
@@ -5,8 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.chinh.streamingservice.mediapersistence.projection.MediaSearchItem;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Service;
 import java.time.Duration;
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
@@ -29,10 +31,26 @@ public class MediaSearchCacheService {
         String id = "media::" + item.getId();
         try {
             String json = objectMapper.writeValueAsString(item);
-            redisStringTemplate.opsForValue().set(id, json, duration);
+            setOrRefreshTtl(id, json, duration.getSeconds());
         } catch (JsonProcessingException e) {
             throw new RuntimeException("Failed to parse json", e);
         }
+    }
+
+    public void setOrRefreshTtl(String key, String value, long timeoutInSeconds) {
+        String luaScript =
+                "if redis.call('EXISTS', KEYS[1]) == 1 then " +
+                "    return redis.call('EXPIRE', KEYS[1], ARGV[2]) " + // If exists, just update TTL
+                "else " +
+                "    return redis.call('SET', KEYS[1], ARGV[1], 'EX', ARGV[2]) " + // If not, set value + TTL
+                "end";
+        DefaultRedisScript<Long> script = new DefaultRedisScript<>(luaScript, Long.class);
+
+        redisStringTemplate.execute(script,
+                Collections.singletonList(key),
+                value,
+                String.valueOf(timeoutInSeconds)
+        );
     }
 
     public MediaSearchItem getCachedMediaSearchItem(long id) {
